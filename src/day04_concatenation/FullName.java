d2b6425060c858f541e725a18f3ad8df9fc0ca13
Umran Ozman <umonal@yahoo.com>
2023-02-09 17:38:41 -0500

    FullName Class created

---
+++ src/day04_concatenation/FullName.java
@@ -0,0 +1,13 @@
+package day04_concatenation;
+
+public class FullName {
+    public static void main(String[] args){
+        String firstName = "Wooden";
+        String lastName = "Spoon";
+
+        String fullName = firstName + " " + lastName;
+
+        System.out.println("Your full name is " + fullName );
+
+    }
+}
