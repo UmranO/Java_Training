8c1cab8897dc7be8bbb45ecb2e34b75dc54eb773
Umran Ozman <umonal@yahoo.com>
2023-02-09 15:36:49 -0500

    ReplaceVariableName Class created

---
+++ src/day03_variables/ReplaceVariableName.java
@@ -0,0 +1,29 @@
+package day03_variables;
+
+public class ReplaceVariableName {
+
+
+    public static void main(String[] args) {
+
+        String student_name = "Josh";
+
+        System.out.println(student_name);  // Control+r is used to replace smtg in the code
+        System.out.println(student_name);  // OR
+        System.out.println(student_name);  // Highlight+Right Click+Refactor+Rename+changedValue+enter
+        System.out.println(student_name);
+        System.out.println(student_name);
+
+
+        int salary = 100000;
+
+        System.out.println(salary);
+        System.out.println(salary);
+        System.out.println(salary);
+        System.out.println(salary);
+        System.out.println(salary);
+
+
+    }
+
+
+}
