6a3af52c8c65933834591d31d0a5192400ee6c8a
Umran Ozman <umonal@yahoo.com>
2023-02-08 21:49:21 -0500

    EmployeeInfo class created

---
+++ src/day02_escapeSequences/EmployeeInfo.java
@@ -0,0 +1,30 @@
+package day02_escapeSequences;
+
+public class EmployeeInfo {
+    public static void main (String [] args){
+       /*
+    comment 1
+    comment 2
+    comment 3
+     */
+System.out.println("Name: Umran Ozman ");
+System.out.println("Gender: Female");
+System.out.println("Age: 52");
+System.out.println("Job Title: Java Developer");
+System.out.println("Company: Cydeo");
+System.out.println("Salary: $140000");
+
+    }
+}
+
+/*
+4. Create a Java file named "EmployeeInfo" and write a program that can display the following output on the console:
+			output:
+				Name: $yourName
+				Gender: $gender
+				Age: $age
+				Job Title: Java Developer
+				Company: $companyName
+				Salary: $salary
+			Note: Put your own info for name, gender etc
+ */
