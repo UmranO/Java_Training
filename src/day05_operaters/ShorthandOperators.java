53ceb55aec125b8baf8c608ad343466d33850b5d
Umran Ozman <umonal@yahoo.com>
2023-02-12 10:50:09 -0500

    Shorthand Operators Class created

---
+++ src/day05_operaters/ShorthandOperators.java
@@ -0,0 +1,24 @@
+package day05_operaters;
+
+public class ShorthandOperators {
+
+    public static void main(String[] args) {
+
+        int a = 20;
+
+        System.out.println(a); //20
+
+        System.out.println(a); //20
+
+        a = 40;
+
+        System.out.println(a);
+
+        a = 90;
+
+        System.out.println(a);
+
+
+    }
+
+}
