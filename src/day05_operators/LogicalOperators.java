255b6e48308bef5d8f697df239de793d8eb591c9
Umran Ozman <umonal@yahoo.com>
2023-02-12 12:52:25 -0500

    LogicalOperators Class updated

--- src/day05_operators/LogicalOperators.java
+++ src/day05_operators/LogicalOperators.java
@@ -40,7 +40,41 @@ public class LogicalOperators {
 
         System.out.println(validAnswer);
 
+        System.out.println("-----------------------------------------------------");
 
+        char grade = 'A';
+
+        boolean passedTheExam = grade == 'A' || grade == 'B' || grade == 'C' || grade == 'D';
+
+        System.out.println(passedTheExam);
+
+
+        System.out.println("-----------------------------------------------------");
+
+        System.out.println(!true);
+
+        String a = "yes";
+
+        boolean yes =  a == "yes"; // true
+        boolean no = !yes; // false
+
+        System.out.println("yes = " + yes);
+        System.out.println("no = " + no);
+
+        System.out.println("-----------------------------------------------------");
+
+        int score = 65;
+
+        boolean passed = score >= 60;
+        boolean failed =  !passed;
+
+        System.out.println("passed = " + passed);
+        System.out.println("failed = " + failed);
+
+        System.out.println("-----------------------------------------------------");
+
+        System.out.println(  true == !false && false  == !true && true != !true  );
+        //                    true          &&     true        &&   true
 
     }
 
