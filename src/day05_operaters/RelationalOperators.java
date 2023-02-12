7c5ae6e03e6390c249bace4d2d5638e44211fefd
Umran Ozman <umonal@yahoo.com>
2023-02-12 11:41:48 -0500

    RelationalOperators Class updated

--- src/day05_operaters/RelationalOperators.java
+++ src/day05_operaters/RelationalOperators.java
@@ -36,7 +36,28 @@ public class RelationalOperators {
         System.out.println( 100 > 100); // false
         System.out.println( 100 >= 100); // true
         System.out.println(100 >= 85); // true
+        System.out.println("------------------------------------------");
 
+        score = 48;
+
+        boolean failed = score < 60;
+
+        System.out.println(failed);
+
+        System.out.println(1000 < 10000); // true
+
+        System.out.println("------------------------------------------");
+
+        System.out.println( 95 <= 100);  // true
+
+        System.out.println( 100 <= 100); // true
+
+        System.out.println("------------------------------------------");
+
+        // System.out.println("Java" <= "C#"); // >, <, <=, >= can only be applicable for numbers
+
+        System.out.println( 'a' > 'b');
+        //                  65  >  66
 
 
     }
