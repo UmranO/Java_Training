ea47bfc15bc76cb7f8d67a47ebd85366b8b4d79f
Umran Ozman <umonal@yahoo.com>
2023-02-12 11:25:38 -0500

    RelationalOperators Class updated

--- src/day05_operaters/RelationalOperators.java
+++ src/day05_operaters/RelationalOperators.java
@@ -1,4 +1,26 @@
 package day05_operaters;
-
 public class RelationalOperators {
+
+    public static void main(String[] args) {
+
+        int a =  1000;
+        int b = 200;
+
+        //System.out.println(  a > b  );
+        boolean aIsGreater = a > b;
+
+        System.out.println(aIsGreater);
+
+        System.out.println("-----------------------------------");
+
+        int score = 75;
+
+        boolean passed = score >= 60;
+
+        System.out.println(passed);
+
+        System.out.println("---------------------------------");
+
+
+    }
 }
