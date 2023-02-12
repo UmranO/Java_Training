d39087cc9e8e6341111511d7a441b887d8025e71
Umran Ozman <umonal@yahoo.com>
2023-02-12 11:59:45 -0500

    LogicalOperators Class updated

--- src/day05_operators/LogicalOperators.java
+++ src/day05_operators/LogicalOperators.java
@@ -1,4 +1,29 @@
 package day05_operators;
 
 public class LogicalOperators {
+
+    public static void main(String[] args) {
+
+        double salary = 60000;
+        int creditScore = 650;
+        int age = 42;
+
+        boolean eligibleForLoan =   salary >= 30000 && creditScore >= 650 && age >= 18;
+        //  true        &&     true            &&   true
+
+        System.out.println(eligibleForLoan);
+
+        System.out.println("-----------------------------------------------------");
+
+        age = 18;
+        String country = "USA";
+
+        boolean eligibleToVote = age >= 18  && country == "USA";
+        //                          true    &&  false
+
+        System.out.println(eligibleToVote);
+
+
+    }
+
 }
