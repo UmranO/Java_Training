13d65112eb68be72f0c6c2411b91893a134d11ea
Umran Ozman <umonal@yahoo.com>
2023-02-12 10:52:56 -0500

    Shorthand Operators Class updated

--- src/day05_operaters/ShorthandOperators.java
+++ src/day05_operaters/ShorthandOperators.java
@@ -18,6 +18,30 @@ public class ShorthandOperators {
 
         System.out.println(a);
 
+        System.out.println("------------------------------------------");
+
+        double balance = 100;
+
+        balance += 1000; // balance = 100 + 1000
+
+        System.out.println("balance = " + balance);
+
+        balance += 500; // balance= 1100 + 500
+
+        System.out.println("balance = " + balance);
+
+        balance += 10000;
+
+        System.out.println("balance = " + balance);
+
+        balance -= 1000;  // balance = 11600 - 1000
+
+        System.out.println("balance = " + balance);
+
+        balance -= 5000;
+
+        System.out.println("balance = " + balance);
+
 
     }
 
