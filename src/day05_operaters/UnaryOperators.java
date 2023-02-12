7b776a2549f3b594d902711459e977e2626ceace
Umran Ozman <umonal@yahoo.com>
2023-02-12 10:28:19 -0500

    UnaryOperators Class updated-Comments added

--- src/day05_operaters/UnaryOperators.java
+++ src/day05_operaters/UnaryOperators.java
@@ -5,8 +5,8 @@ public class UnaryOperators {
 // pre increment/decrement
         int x = 10;
 
-        System.out.println( ++x ); //11
-        System.out.println(x);     //11
+        System.out.println( ++x ); //11  pre'yi uygular uygulamaz deger degisiyor
+        System.out.println(x);     //11  tekrar print edersen deger hala 11
 
 
         int y = 100;
@@ -25,16 +25,19 @@ public class UnaryOperators {
 
         int b = 25;
 
-        System.out.println( b-- ); //25
-        System.out.println(b);     //24
+        System.out.println( b-- ); //25 it will postpone the decreasing by 1 first & print current value
+        System.out.println(b);     //post means 2 steps will be executed: 1st step pass the current value then
+                                   //change the value right away
 
 
         System.out.println("------------------------------------");
 
         int n = 30;
 
-        int m = n++;  // m =  30, n=31   // post'tan sonraki ilk kullanim mevcut deger sonra hemen artiyor  or eksiliyor.
-
+        int m = n++;  // m =  30, n=31  //post'tan sonraki ilk kullanim mevcut deger sonra hemen artiyor  or eksiliyor
+                                        //suppose we're assigning post increment of n to m
+                                        // post mean the current value is assigned to m then the 2nd step n's value
+                                        // will be increased to 31
         System.out.println("n = " + n);
         System.out.println("m = " + m);
 
