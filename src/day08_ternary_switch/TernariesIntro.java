1ba33e3be0509e30e298a10f5513c0e810044840
Umran Ozman <umonal@yahoo.com>
2023-02-19 11:12:19 -0500

    TernariesIntro Class updated

--- src/day08_ternary_switch/TernariesIntro.java
+++ src/day08_ternary_switch/TernariesIntro.java
@@ -19,5 +19,22 @@ public class TernariesIntro {
         String result3=(score>=60)? "Passed" : "Failed";
         System.out.println(result3);
 
+        System.out.println("----------------------------------------------------");
+
+        int age = 34;
+
+        /*
+        String r;
+        if(age >= 21){
+            r = "Eligible";
+        }else{
+            r = "Not eligible";
+        }
+    */
+
+        String result4 = (age>=21)? "Elligible" : "Not Elligible";
+
+        System.out.println(result4);
+
     }
 }
