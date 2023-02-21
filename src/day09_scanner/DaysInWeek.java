10a137bb0c3631091fa1542866a2d446445d5951
Umran Ozman <umonal@yahoo.com>
2023-02-21 11:41:18 -0500

    DaysInWeek Class created

--- src/day09_scanner/DaysInWeek.java
+++ src/day09_scanner/DaysInWeek.java
@@ -5,9 +5,12 @@ import java.util.Scanner;
 public class DaysInWeek {
     public static void main(String[] args){
 
-        Scanner input=new Scanner (System.in);
+        Scanner input=new Scanner (System.in);        //If we'll use scanner object once then no need to store it into
+                                                      //variable. Declaring variable is not necessary at all
         System.out.println("Enter the day number:");
-        //int num = new Scanner(System.in).nextInt();
+                                           //We can directly call the () from the object new Scanner(System.in.nextInt()
+                                           //and also assign it int num = new Scanner(System.in).nextInt();
+                                           //Only if we'll use it once & if we go this way no need to close the scanner
         int numD = input.nextInt();
 
         String day = "Invalid";
