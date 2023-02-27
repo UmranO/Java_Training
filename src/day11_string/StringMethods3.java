ff50cc2ff694baa70aad6ad3c32300195c05a1ae
Umran Ozman <umonal@yahoo.com>
2023-02-27 17:24:05 -0500

    StringMethods3 Class created

---
+++ src/day11_string/StringMethods3.java
@@ -0,0 +1,28 @@
+package day11_string;
+
+public class StringMethods3 {
+    public static void main(String[] args) {
+
+        String wordW = "";
+
+        boolean r1 = wordW.isEmpty();
+
+        System.out.println(r1);
+
+        // -------------------------
+
+        String str = "        ";
+
+        boolean r2 = str.isEmpty();
+        boolean r3 = str.isBlank();
+
+        System.out.println("r2 = " + r2);
+        System.out.println("r3 = " + r3);
+
+        //------------------------------------
+
+
+    }
+
+}
+
