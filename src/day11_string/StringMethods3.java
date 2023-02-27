b9639d49df0c7cbf40d493794994d0c2eda6a1d3
Umran Ozman <umonal@yahoo.com>
2023-02-27 17:28:04 -0500

    StringMethods3 Class updated

--- src/day11_string/StringMethods3.java
+++ src/day11_string/StringMethods3.java
@@ -20,6 +20,28 @@ public class StringMethods3 {
         System.out.println("r3 = " + r3);
 
         //------------------------------------
+        String str1 = "Cydeo";
+
+        String str2 = new String("Cydeo");
+
+        System.out.println(str1 == str2);
+
+        System.out.println( str1.equals(str2) );
+
+        String str3 = new String("cydeo");
+
+        System.out.println( str2.equals(str3));
+
+        //------------------------------------
+
+        String s1 = "JAVA";
+        String s2 = "java";
+
+        System.out.println(s1.equals(s2));           //false          to get the exact one
+        System.out.println(s1.equalsIgnoreCase(s2)); // true          to get the same letters ignoring if capital or NOT
+
+        //------------------------------------
+        // yes , Yes, yES, YeS  ===> yes
 
 
     }
