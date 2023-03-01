e23c5bb53c39830e9a8c1ffb42e4018f6b61e5de
Umran Ozman <umonal@yahoo.com>
2023-03-01 14:07:16 -0500

    FormatFullName Class updated

--- src/day12_customMethods/FormatFullName.java
+++ src/day12_customMethods/FormatFullName.java
@@ -3,7 +3,7 @@ import java.util.Locale;
 import java.util.Scanner;
 public class FormatFullName {
     public static void main(String[] args){
-  //UO-----------------------------------------------------------------------------------------------------------------
+  /*UO-----------------------------------------------------------------------------------------------------------------
         Scanner input=new Scanner (System.in);
         System.out.println("Enter your name");
         String name=input.nextLine().toLowerCase();
@@ -12,27 +12,36 @@ public class FormatFullName {
 
         System.out.println(name.replace((name.charAt(0)+""),(name.charAt(0)+"").toUpperCase())+" "
                 +(lastName.replace((lastName.charAt(0)+""),(lastName.charAt(0)+"").toUpperCase())));
-
+*/
   //M-----------------------------------------------------------------------------------------------------------------
 
-        //--Without Scanner----------------------
-        String first="jAva";
-        String last="SCHOOL";
+        //--With Scanner-------------------------
 
-        first = first.substring(0, 1).toUpperCase() + first.substring(1).toLowerCase();
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter your first name:");
+        String first = input.nextLine().trim().replace(" ", "");
+        // trim(0 & replace() methods will make sure that the white spaces & additionals spaces between the characters
+        // will be removed
+
+        System.out.println("Enter your last name:");
+        String last = input.nextLine().trim().replace(" ", "");
+
+        input.close();
+
+       //--Without Scanner----------------------
+
+        String first1="jAva";
+        String last1="SCHOOL";
+
+        first1 = first1.substring(0, 1).toUpperCase() + first1.substring(1).toLowerCase();
         //getting first character of first name  +  the remaining characters of the first name
 
-        last = last.substring(0, 1).toUpperCase() + last.substring(1).toLowerCase();
+        last1 = last1.substring(0, 1).toUpperCase() + last1.substring(1).toLowerCase();
 
-        String full_name = first + " " + last;
+        String full_name = first1 + " " + last1;
 
         System.out.println(full_name);
-
-
-
-
-
-
 
     }
 
