6381a66709115f153f9c8086fbf3ae95d46d681e
Umran Ozman <umonal@yahoo.com>
2023-03-02 21:07:28 -0500

    Test Class updated

--- src/day12_customMethods/Test.java
+++ src/day12_customMethods/Test.java
@@ -1,8 +1,20 @@
 package day12_customMethods;
 
 public class Test {
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
+
+        int number = 300;
+
+        CustomMethodsWithParameter.oddOrEven(300);    //This custom () oddOrEven is created in
+                                                              //CustomMethodsWithParameter Class
+
+        String firstName = "Vasyl",
+                lastName = "Dobrianski";
+
+        CustomMethodsPractice.initials(firstName, lastName);
 
 
     }
+
 }
