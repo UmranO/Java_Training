0a4c5f949347dff1d95005306957dbc8586aad7d
Umran Ozman <umonal@yahoo.com>
2023-03-07 10:29:51 -0500

    Warmup Class updated / Switch implemented

--- src/day13_customMethods/WarmupTasks.java
+++ src/day13_customMethods/WarmupTasks.java
@@ -1,4 +1,80 @@
 package day13_customMethods;
 
 public class WarmupTasks {
-}
+    public static void main(String[] args) {
+
+        calculateSwitch(1, 2, '!');
+
+    }
+
+    public static void calculateSwitch(double num1, double num2, char mathOperator) {
+
+
+        switch (mathOperator) {
+
+            case '+':
+                System.out.println(num1 + " " + mathOperator + " " + num2 + " = " + (num1 + num2));
+                break;
+
+            case '-':
+                System.out.println(num1 + " " + mathOperator + " " + num2 + " = " + (num1 - num2));
+                break;
+
+            case '*':
+                System.out.println(num1 + " " + mathOperator + " " + num2 + " = " + (num1 * num2));
+                break;
+
+            case '/':
+                System.out.println(num1 + " " + mathOperator + " " + num2 + " = " + (num1 / num2));
+                break;
+
+            default:
+                System.err.println("Invalid operator: " + mathOperator);
+
+        }
+
+
+    }}
+
+
+
+
+
+/*
+Warmup task:
+	1. Create a method named calculate that can take three arguments:
+            num1 (double)
+            num2 (double)
+            mathOperator (char)
+
+	    The method should display the calculation result of those two numbers as long as the 3rd argument is a valid
+	    math operator, otherwise the method prints "Invalid Operator"
+
+	        Ex:
+	            calculate(10, 20, '+')
+
+	        output:
+	            10 + 20 = 30
+
+
+	2. Create a method named ageGroup that can print the age group of the person
+
+	        age groups are:
+	                Teenager (< 21)
+	                Adult   (>=21 && <55 )
+	                Senior  ( > 55 )
+
+
+    3. Create a method named eligibleToVote that takes two arguments:
+	        1. age (int)
+	        2. isAmerican (boolean)
+
+	    Then the method should determine if the person eligible to vote
+	        Ex:
+	            eligibleToVote(23, true)
+
+	        output:
+	            You are eligible to vote
+
+
+ */
