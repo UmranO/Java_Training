bfef437162e6ea1ccc63e1ca28a4c676c9404104
Umran Ozman <umonal@yahoo.com>
2023-03-08 19:56:22 -0500

    ForLoopPractice2 Class updated

--- src/day14_forLoop/ForLoopPractice2.java
+++ src/day14_forLoop/ForLoopPractice2.java
@@ -1,4 +1,20 @@
 package day14_forLoop;
-
+import java.util.Scanner;
 public class ForLoopPractice2 {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);                  //Scanner object should be created outside the loop
+
+        int sum=0;                                               //Ask 5 # and display the sum
+        for(int i=1; i<=5; i++) {
+
+            System.out.println("Enter a number");
+            sum+=input.nextInt();
+
+        }
+        System.out.println(sum);
+
+
+    }
 }
