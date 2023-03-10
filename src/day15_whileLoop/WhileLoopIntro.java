6414c951ec533e777df022708c3ddebce16b9588
Umran Ozman <umonal@yahoo.com>
2023-03-10 09:20:53 -0500

    WhileLoopIntro Class updated

--- src/day15_whileLoop/WhileLoopIntro.java
+++ src/day15_whileLoop/WhileLoopIntro.java
@@ -1,4 +1,24 @@
 package day15_whileLoop;
 
 public class WhileLoopIntro {
+    public static void main (String [] args){
+
+//        if(true){
+//            System.out.println("Hello World");
+//        }
+//
+//        System.out.println("----------------------------------------------");
+//
+//        for (int i = 0; i < 10; i++) {
+//
+//        }
+//
+//        System.out.println("----------------------------------------------");
+
+        while(true){                                                                    //This creates an infinite loop
+            System.out.println("Hello Cydeo");
+        }
+
+    }
+
 }
