566de747d0bc1468028f27b61f1c71585b411a0b
Umran Ozman <umonal@yahoo.com>
2023-03-09 20:04:17 -0500

    ContinueStatement Class updated

--- src/day15_whileLoop/ContinueStatement.java
+++ src/day15_whileLoop/ContinueStatement.java
@@ -6,8 +6,8 @@ public class ContinueStatement {
         for (int i = 0; i < 6; i++) {
             if (i == 2) {
                 continue;}
-            System.out.println(i);                         //Bunu if statement'da verirsen erisilmez oluyor!
-        }                                                      //If we don't want this statement to be executed in a
+            System.out.println(i);                              //Bunu if statement'da verirsen erisilmez oluyor!
+        }                                                       //If we don't want this statement to be executed in a
         //specific iteration. eg skip this sout in iteration 4
         System.out.println("----------------------------------------------------------------------------------------");
 
@@ -34,11 +34,41 @@ public class ContinueStatement {
             System.out.println(i);
         }
         System.out.println("----------------------------------------------------------------------------------------");
-/*
-        for (int i=0; i< 3; i++)
-            System.out.println("Hello World");     //if it has only 1 statement in its body it can be given without {}
-            System.out.println("Hello Cydeo");     //This sout is not in for loop's body now when no {} is present so
-                                                   //this statement won't be repeated 3 times will only be printed once
-*/
+//
+//        for (int i=0; i< 3; i++)
+//            System.out.println("Hello World");     //if it has only 1 statement in its body it can be given without {}
+//            System.out.println("Hello Cydeo");     //This sout is not in for loop's body now when no {} is present so
+//                                                   //this statement won't be repeated 3 times will only be printed once
+//
+        System.out.println("----------------------------------------------------------------------------------------");
+        // find the sum of all the even number between 50 ~ 100
+
+        for (int i = 50; i < 101; i++) {                            //Alternative-1 to display even numbers btw 50 ~100
+            if (i % 2 == 0) {
+                System.out.print(i +" ");
+            }
+        }
+        System.out.println();                                      //To append a new line otherwise it will print back
+                                                                   //to back
+        System.out.println("---------------------------------");
+
+        //50, 52, 54, 56 ...... 100                                //Alternative-2 to display even numbers btw 50 ~100
+        for (int i = 50; i < 101; i += 2) {                        //Bunda 2'ser ekleyerek ilerliyoruz.
+            System.out.print(i+" ");
+        }
+        System.out.println();
+
+        System.out.println("----------------------------------------------------------------------------------------");
+
+
+        int sum=0;
+        for (int i = 50; i <=100 ; i++) {
+            if (i%2!=0) {                                          //if the i's value is odd #
+                 continue;}                                        //skip it
+            else {sum+=i;}
+
+        }System.out.println(sum);
+   
+   
     }
 }
