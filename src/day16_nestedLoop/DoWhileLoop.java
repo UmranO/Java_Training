feae23a41fd70200bd86398fe5167d1d20013cfd
Umran Ozman <umonal@yahoo.com>
2023-03-11 11:39:36 -0500

    DoWhileLoop Class updated

--- src/day16_nestedLoop/DoWhileLoop.java
+++ src/day16_nestedLoop/DoWhileLoop.java
@@ -1,4 +1,27 @@
 package day16_nestedLoop;
 
 public class DoWhileLoop {
+    public static void main(String[] args) {
+
+        boolean condition = false;
+
+        //In for loop if the condition is false "Hello..." won't be printed.  Will be printed 0 times!
+        for (; condition ;){
+            System.out.println("Hello Cydeo   --- For Loop");
+        }
+        System.out.println("-------------------------------------");
+
+        //In while loop if the condition is false "Hello..." won't be printed.  Will be printed 0 times!
+        while(condition){
+            System.out.println("Hello Cydeo  --- While Loop");
+        }
+
+        System.out.println("-------------------------------------");
+
+      //In do while loop if the condition is false "Hello..." won't be printed.  Will be printed 1 times!
+        do{
+            System.out.println("Hello Cydeo  --- Do&While Loop");
+        }while (condition);
+
+    }
 }
