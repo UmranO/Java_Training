c415e8d2e77464b0d9baf5c3b7d819718e6086a8
Umran Ozman <umonal@yahoo.com>
2023-03-20 12:09:59 -0400

    WarmaUpTasks Class updated

--- src/day18_garbageCollection/WarmUpTasks_UO.java
+++ src/day18_garbageCollection/WarmUpTasks_UO.java
@@ -2,12 +2,22 @@ package day18_garbageCollection;
 public class WarmUpTasks_UO {
     public static void main(String[] args) {
 //----Test-----------------------------------------------------------------------------------------------------
-//----Test Pizza-------------------------------------
-Pizza_UO pizza=new Pizza_UO();
-pizza.setInfo('L',2,3);
+
+//----Test Pizza_UO-------------------------------------
+        Pizza_UO pizza=new Pizza_UO();
+        pizza.setInfo('L',2,3);
         System.out.println(pizza);
 
 
+//----Test BankAccount_UO------------------------------
+        BankAccount_UO account=new BankAccount_UO();
+
+        account.setInfo("UO",45618431387613l,10000.00);
+        System.out.println(account);
+        account.checkBalance();
+        System.out.println(account.deposit(100));
+        System.out.println(account.withdraw(200.0));
 
     }
+
 }
