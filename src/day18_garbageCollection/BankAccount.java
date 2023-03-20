1913e45e1263d244510bf5610027a2cab9a0537a
Umran Ozman <umonal@yahoo.com>
2023-03-20 11:55:12 -0400

    BankAccount Class updated

--- src/day18_garbageCollection/BankAccount.java
+++ src/day18_garbageCollection/BankAccount.java
@@ -2,3 +2,17 @@ package day18_garbageCollection;
 
 public class BankAccount {
 }
+/*
+2. Create a custom class named BankAccount:
+			Attributes:
+				1. accountHolder
+				2. accountNumber
+				3. balance
+
+			Actions:
+				1. setInfo(): sets the accountHolder and accountNumber attributes
+		        2. toString(): displays the full account info
+		        3. checkBalance():displays the available balance
+		        4. deposit(amount): increases the balance by the specified amount
+		        5. withdraw(amount): decreases the balance by the specified amount
+ */
