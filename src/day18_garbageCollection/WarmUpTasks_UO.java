f0d083bf40589d79f4ed192f056b47be236ded24
Umran Ozman <umonal@yahoo.com>
2023-03-20 11:42:27 -0400

    WarmUpTasks Class updated

--- src/day18_garbageCollection/WarmUpTasks_UO.java
+++ src/day18_garbageCollection/WarmUpTasks_UO.java
@@ -1,4 +1,13 @@
 package day18_garbageCollection;
-
 public class WarmUpTasks_UO {
+    public static void main(String[] args) {
+//----Test-----------------------------------------------------------------------------------------------------
+//----Test Pizza-------------------------------------
+Pizza_UO pizza=new Pizza_UO();
+pizza.setInfo('L',2,3);
+        System.out.println(pizza);
+
+
+
+    }
 }
