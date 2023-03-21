af38909eebdc3e2e5547bfef2cb9c3a32638ec0c
Umran Ozman <umonal@yahoo.com>
2023-03-21 09:27:36 -0400

    TestBankAccountObjects Class updated

--- src/day18_garbageCollection/TestBankAccountObjects.java
+++ src/day18_garbageCollection/TestBankAccountObjects.java
@@ -2,6 +2,7 @@ package day18_garbageCollection;
 
 public class TestBankAccountObjects {
     public static void main(String[] args){
+
         BankAccount obj1 = new BankAccount();
         obj1.setInfo("Gadir", 123456789);
 
@@ -18,7 +19,41 @@ public class TestBankAccountObjects {
         obj1.checkBalance();
 
 
+        obj1.deposit(-1000);
+
+        obj1.checkBalance();
+
+        obj1.withdraw(5000);
+
+        obj1.checkBalance();
+
+
+        System.out.println("-------------------------------------------");
+
+        BankAccount account1 = new BankAccount();
+
+        BankAccount account2 = new BankAccount();
+
+        account1.setInfo("Sebastian", 789456123);
+
+        account2.setInfo("Umran", 456789123);
+
+
+        account1.deposit(5000);
+        account2.deposit(10000);
+
+        System.out.println(account1);
+        System.out.println(account2);
+
+
+        account1.withdraw(1000);
+
+
+        System.out.println(account1);
+        System.out.println(account2);
+
 
 
     }
+
 }
