21ae6d222bc6aed1bb1d304d9809fd30fd7491c1
Umran Ozman <umonal@yahoo.com>
2023-03-22 21:25:57 -0400

    MaxNumber updated

--- src/day19_array/MaxNumber.java
+++ src/day19_array/MaxNumber.java
@@ -1,4 +1,24 @@
 package day19_array;
 
 public class MaxNumber {
+    public static void main(String[] args) {
+
+        int[] arr = {100, 20, 500, 40, -10, 30};
+        int max=arr[0];                              //assume that first element is the maximum number
+
+        for (int i = 0; i < arr.length; i++) {       //i: 1, 2, 3, 4...
+           
+            if (arr[i] > max){                       //compares the element of the array with current maximum number
+                max=arr[i];}                         //replace the current maximum number
+        }
+        System.out.println("max = " + max);
+
+    }
+
 }
+
+/*
+Task:
+	1. Write a program that can find the maximum number from an array
+
+ */
