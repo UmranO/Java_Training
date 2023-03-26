2ccc178a78920395e2820e9b83dfabf648e82add
Umran Ozman <umonal@yahoo.com>
2023-03-26 15:35:06 -0400

    AverageNumber_UO Class updated

--- src/day19_array/AverageNumber_UO.java
+++ src/day19_array/AverageNumber_UO.java
@@ -1,4 +1,5 @@
 package day19_array;
+import java.text.DecimalFormat;
 import java.util.Scanner;
 public class AverageNumber_UO {
     public static void main(String[] args){
@@ -20,7 +21,14 @@ public class AverageNumber_UO {
         }                                       //usually the average # will be a decimal so declare double average
         double average=sum/(double)num;         //int/int will give us an int so if we want to get a decimal we need to
         System.out.println(average);            //change either numerator or denominator to decimal so we can cast one
-        input.close();                          //of them to decimal. If we don't cast we'll get smtg like 10.0 or num.0
+                                                //of them to decimal. If we don't cast we'll get smtg like 10.0 or num.0
+        DecimalFormat df =new DecimalFormat("0.00"); //We can use decimal formater to limit the numbers after .
+                                                            //1st we need to have the object of the DecimalFormat Class
+                                                            //Usually df is given as the variable name
+                                                           //We can give the pattern in the new object of the DecimalFo.
+        System.out.println("average: " + df.format(average) ); //than from this object we can call the format() & in ()
+                                                               //we can pass the number we want to format
+        input.close();                                         //This won't change the # but only show the # as we want
     }
 }
 /*	5. AverageNumber:
