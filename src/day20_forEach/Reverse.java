0957ec361aa3f7e40eb522d884c14d0d7d1fa3ae
Umran Ozman <umonal@yahoo.com>
2023-03-26 16:16:15 -0400

    Reverse Class updated

--- src/day20_forEach/Reverse.java
+++ src/day20_forEach/Reverse.java
@@ -2,3 +2,10 @@ package day20_forEach;
 
 public class Reverse {
 }
+/*
+3. Write a program that can reverse an array of integers and returns it as new array
+	        ex:
+	            array = {1,2,3,4,5};
+	        output:
+	            reversedArray = {5,4,3,2,1};
+ */
