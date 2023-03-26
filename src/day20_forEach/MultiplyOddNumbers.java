e90646c159ab78d83092e9189bdc77f6719b5412
Umran Ozman <umonal@yahoo.com>
2023-03-26 16:15:47 -0400

    MultiplyOddNumbers Class updated

--- src/day20_forEach/MultiplyOddNumbers.java
+++ src/day20_forEach/MultiplyOddNumbers.java
@@ -2,3 +2,10 @@ package day20_forEach;
 
 public class MultiplyOddNumbers {
 }
+/*
+1. write a program that can multiply each odd number of an integer array by 2
+		            ex:
+		            	array = [1,2,3,4,5];
+	                output:
+	                	array =[2,2,6,4,10]
+ */
