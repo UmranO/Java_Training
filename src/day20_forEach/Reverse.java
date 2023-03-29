3fe19fd0917bf0a1d8954559606e73b80ac6ed39
Umran Ozman <umonal@yahoo.com>
2023-03-29 08:38:21 -0400

    Reverse Class updated

--- src/day20_forEach/Reverse.java
+++ src/day20_forEach/Reverse.java
@@ -1,6 +1,19 @@
 package day20_forEach;
 
+import java.util.Arrays;
+
 public class Reverse {
+    public static void main(String[] args){
+//----UO Way-------------------------------------------------------------------------------------------
+       int[] array = {1,2,3,4,5};
+       int[] reverse=new int[array.length];
+
+       for(int i =array.length-1, j=0; i>=0; i--, j++)
+           reverse[j] = array[i];
+        System.out.println(Arrays.toString(reverse));
+
+
+    }
 }
 /*
 3. Write a program that can reverse an array of integers and returns it as new array
