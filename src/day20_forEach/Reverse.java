cd5250f00ed9ae309cac6453c2299314c2f688cd
Umran Ozman <umonal@yahoo.com>
2023-03-29 08:40:38 -0400

    Reverse Class updated

--- src/day20_forEach/Reverse.java
+++ src/day20_forEach/Reverse.java
@@ -13,6 +13,16 @@ public class Reverse {
         System.out.println(Arrays.toString(reverse));
 
 
+//----M Way-------------------------------------------------------------------------------------------
+        int[] arr = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
+
+        int[] reverse1 = new int[arr.length]; // {5, 4, 3, 2, 1}
+
+        for (int i = arr.length - 1, j=0; i >= 0; i--) {        //j'yi burada declare etmis ama
+            reverse1[j++]= arr[i];                              //artisi burada yapiyor, j++ oldugu icin de ilk once 0,
+        }                                                       //sonraki turda 1 oluyor
+        System.out.println(Arrays.toString(reverse1));
+
     }
 }
 /*
