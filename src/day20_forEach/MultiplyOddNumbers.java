b55a99e7559fe78539197d44b5f9f34d0b6c7f0e
Umran Ozman <umonal@yahoo.com>
2023-03-29 08:57:26 -0400

    MultiplyOddNumbers Class updated

--- src/day20_forEach/MultiplyOddNumbers.java
+++ src/day20_forEach/MultiplyOddNumbers.java
@@ -8,8 +8,8 @@ public class MultiplyOddNumbers {
         int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
         for (int i = 0; i < array.length; i++) {
-          if (array[i]%2 !=0){array[i]= array[i] * 2;
-          }
+          if (array[i]%2 !=0){array[i]= array[i] * 2;        //should be multiplied by 2 & assigned back to the element
+          }                                                  //at that index
         }
         System.out.println(Arrays.toString(array));
 
@@ -19,12 +19,12 @@ public class MultiplyOddNumbers {
 
         for (int i = 0; i < numbers.length; i++) {
 
-            if(numbers[i]%2 ==0){
-                continue;
+            if(numbers[i]%2 ==0){                          //if we don't give this condition all will be multiplied by 2
+                continue;                                  //but we want only the odd values to be multiplied by 2
             }
-            numbers[i] = numbers[i] * 2;
-        }
-
+            numbers[i] = numbers[i] * 2;                   //get the element by numbers[i] & multiply it
+        }                                                  //after multiplying it with 2 assign the new value  it to
+                                                           //that index
         System.out.println( Arrays.toString(numbers) );
 
     }
