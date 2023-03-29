62e342e98d6ad6a5de6260dd4005d002cab60f01
Umran Ozman <umonal@yahoo.com>
2023-03-29 14:24:38 -0400

    MergeTwoArrays Class updated

--- src/day20_forEach/MergeTwoArrays.java
+++ src/day20_forEach/MergeTwoArrays.java
@@ -18,6 +18,25 @@ public class MergeTwoArrays {
         System.out.println(Arrays.toString(arr3));
 
 //----M Way-------------------------------------------------------------------------------------------
+        int[] arr1M = {1,2,3,4};
+        int[] arr2M = {5,6,7};
+
+        int[] arr3M = new int[arr1.length + arr2.length];           //to make sure that 3rd array has enough capacity to
+                                                                    //contain all the elements of arr1 and arr2
+        int k = 0 ;                                                 //for third array's index number
+
+        for (int i = 0; i < arr1.length; i++, k++) {                //i: for first array's index numbers
+            arr3[k] = arr1[i];
+        }
+        for (int i = 0; i < arr2.length; i++, k++) {                //i: for second array's index numbers
+            arr3[k] = arr2[i];
+        }
+
+        System.out.println(Arrays.toString(arr3));
+
+        System.out.println("--------------------------------------------------");
+
+
     }
 }
 /*
