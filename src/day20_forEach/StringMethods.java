4428092a6838e6909f9951202ff7eeff0dd5ec24
Umran Ozman <umonal@yahoo.com>
2023-03-29 16:32:13 -0400

    StringMethods Class updated

--- src/day20_forEach/StringMethods.java
+++ src/day20_forEach/StringMethods.java
@@ -1,4 +1,16 @@
 package day20_forEach;
-
+import java.util.Arrays;
 public class StringMethods {
+    public static void main(String[] args) {
+        String str="Cydeo School";
+
+        char[] ch=str.toCharArray();                          //No parameter needed. We created a char[] out of a String
+        System.out.println(Arrays.toString(ch));              //Output is: [C, y, d, e, o,  , S, c, h, o, o, l]
+                                                              //if we want to get ris of the space we can use replace()
+                                                              //before toCharArray()
+        str=str.replace(" ","");
+        char[] ch1=str.toCharArray();
+        System.out.println(Arrays.toString(ch1));             //Output is: [C, y, d, e, o, S, c, h, o, o, l]
+
+    }
 }
