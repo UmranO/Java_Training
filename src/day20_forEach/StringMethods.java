c1e47da3eb2e3af7363b39b329ea0beea9ffd058
Umran Ozman <umonal@yahoo.com>
2023-03-29 17:42:15 -0400

    StringMethods Class updated

--- src/day20_forEach/StringMethods.java
+++ src/day20_forEach/StringMethods.java
@@ -1,4 +1,6 @@
 package day20_forEach;
+import utilities.ArraysUtility;
+
 import java.util.Arrays;
 public class StringMethods {
     public static void main(String[] args) {
@@ -13,6 +15,7 @@ public class StringMethods {
         System.out.println(Arrays.toString(ch1));             //Output is: [C, y, d, e, o, S, c, h, o, o, l]
 
 System.out.println("-----------------------------------------------------------------------------------------------");
+
         String str1="Today is a great day to learn java";     //If we want to get every single word out of this String
                                                               //If we want to create a String Array out of this String
         str1.split(" ");                                //So we can split str1 by space bec. words're splited with
@@ -28,6 +31,34 @@ System.out.println("------------------------------------------------------------
 
         String[] word1=str1.split("Python");            //Since no Python st1 was not splited
         System.out.println(Arrays.toString(word1));
+
+System.out.println("-----------------------------------------------------------------------------------------------");
+//----M Way-------------------------------------------------------------------------------------------------------
+        String sentence = "Today is a great day";                            //Expected outcome:Java love I
+                                                                             //Calling reverse() from our ArraysUtility
+        String[] arr =  ArraysUtility.reverse( sentence.split(" ") );  //[Java, love I]-1st split then reverse
+
+        System.out.println(Arrays.toString(arr));
+
+//----M Way 2-------------------------------------------------------------------------------------------------------
+
+        String reversedSentence = ""; //"Java love I"
+        for (int i = 0; i < arr.length; i++) {
+            reversedSentence += arr[i] +" ";
+        }
+
+        System.out.println(reversedSentence);
+
+//----UO Way-------------------------------------------------------------------------------------------------------
+        String sentence1="I love Java";                       //Expected outcome:Java love I
+
+        String[] split=sentence1.split(" ");
+        for (int i = split.length - 1; i >= 0; i--) {
+            System.out.print(split[i]+ " ");
+        }
+
+
+
 
     }
 }
