87f116b7fbf66128f2ce0d522f523fec6c686797
Umran Ozman <umonal@yahoo.com>
2023-03-29 19:24:06 -0400

    ArrayUtilityMethods Class updated

--- src/day20_forEach/ArrayUtilityMethods.java
+++ src/day20_forEach/ArrayUtilityMethods.java
@@ -1,4 +1,16 @@
 package day20_forEach;
-
+import java.util.Arrays;
 public class ArrayUtilityMethods {
+    public static void main(String[] args) {
+
+        int[] score = {70, 100, 80, 90, 65};
+
+        System.out.println(Arrays.toString(score));
+
+        String result = Arrays.toString(score);
+
+        System.out.println(result);
+
+        System.out.println("---------------------------------------");
+    }
 }
