2142e9ad6ec229e21bd8b67b32deacb5bb992135
Umran Ozman <umonal@yahoo.com>
2023-03-29 21:56:12 -0400

    ForEachLoopPractice Class updated

--- src/day20_forEach/ForEachLoopPractice.java
+++ src/day20_forEach/ForEachLoopPractice.java
@@ -14,5 +14,48 @@ public class ForEachLoopPractice {
 
         }
 
+        System.out.println("-------------------------------------------");
+
+        int[] nums = {100, 20, 30, 40, 5, 4, 3, 1, 2, 1000, 300, 500};
+
+        int max = nums[0];                            //We are assuming that the 1st element in this Array is the max
+        int min = nums[0];                            //We are assuming that the 1st element in this Array is the min
+
+        for (int each : nums) {                       //We are checking it with the remaining elements
+
+            if(each > max){                           //If each is bigger than the max then we have to change the max
+                max = each;                           //max should be each
+            }
+
+            if(each < min){
+                min = each;
+            }
+
+        }
+
+        System.out.println("max = " + max);
+
+        System.out.println("min = " + min);
+
+        System.out.println("-------------------------------------------");
+
+        int[] a1 = {10, 20, 30, 40, 50};
+        int[] a2 = {60, 70, 80, 90, 100, 110, 120};
+
+        int[] a3 = new int[a1.length + a2.length];
+
+        int k = 0;
+
+        for (int each : a1) {
+            a3[k++]= each;
+        }
+
+        for (int each : a2) {
+            a3[k++]=each;
+        }
+
+        System.out.println(Arrays.toString(a3));
+
+        System.out.println("-------------------------------------------");
     }
 }
