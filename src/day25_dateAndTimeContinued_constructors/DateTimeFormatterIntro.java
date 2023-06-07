57a520188f0e710924cd10ceba8f4cf24a032194
Umran Ozman <umonal@yahoo.com>
2023-06-07 09:45:13 -0400

    DateTimeFormatterIntro Class updated

--- src/day25_dateAndTimeContinued_constructors/DateTimeFormatterIntro.java
+++ src/day25_dateAndTimeContinued_constructors/DateTimeFormatterIntro.java
@@ -1,4 +1,17 @@
 package day25_dateAndTimeContinued_constructors;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class DateTimeFormatterIntro {
+    public static void main(String[] args) {
+
+        DateTimeFormatter df = DateTimeFormatter.ofPattern("y");
+
+        LocalDate today=LocalDate.now();         //Default pattern is: 2023-06-06
+        System.out.println(today);               //If we want another format then we have to use DateTimeFormatter Class
+
+        System.out.println(today.format(df));    //2023
+
+
+    }
 }
