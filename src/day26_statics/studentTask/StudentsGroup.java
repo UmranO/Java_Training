967e3084a5257ed75ea3d7386dc6ab4bd34582f1
Umran Ozman <umonal@yahoo.com>
2023-06-09 15:54:01 -0400

    StudentsGroup Class updated

--- src/day26_statics/studentTask/StudentsGroup.java
+++ src/day26_statics/studentTask/StudentsGroup.java
@@ -1,4 +1,16 @@
 package day26_statics.studentTask;
+import java.util.ArrayList;
 
 public class StudentsGroup {
+
+    public String groupName;
+    public int groupId;
+    public ArrayList<Student> students = new ArrayList<>();
+
+    public StudentsGroup(String groupName, int groupId) { // creates the StudentsGroup object by setting the groupName and groupId
+        this.groupName = groupName;
+        this.groupId = groupId;
+        // students = new ArrayList<>(); // size : 0
+    }
+
 }
