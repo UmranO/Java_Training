948534be1a80b4e32354b47e27bbd76d5a9e63ec
Umran Ozman <umonal@yahoo.com>
2023-06-13 13:12:56 -0400

    Circle Class updated

--- src/day27_accessModifiers/Circle.java
+++ src/day27_accessModifiers/Circle.java
@@ -1,4 +1,50 @@
 package day27_accessModifiers;
 
 public class Circle {
+    public static double pi=3.14;
+    public double radius;
+    public double diameter;
+
+    public Circle(double radius){
+        this.radius=radius;
+        diameter=2*radius;
+    }
+
+    /*
+       public static double calcArea(){                      // static only accepts statics
+           return radius * radius * pi;
+       }
+       */
+
+    public double calcArea(){
+       return radius*radius*pi;
+    }
+    public double calcPerimeter(){
+        return 2*radius*pi;
+    }
+    public static void printPiValue(){
+        System.out.println("PI value is: "+pi);
+    }
+    public String toString(){
+        return "Circle{radius="+radius+", diameter="+diameter+", pi="+pi+", area="+calcArea()+", perimeter="+calcPerimeter()+'}';
+    }
+
 }
+/*
+Warmup tasks:
+    Create a class named Circle:
+
+		Attributes:
+			radius, diameter, pi
+
+		Add a constructor that can set All the fields (instances)
+
+		Actions:
+			calcArea():      returns the area of Circle
+			calcPerimeter(): returns the perimeter of Circle
+			printPi():       displays PI value
+			toString():      displays the radius, diameter, pi, area and perimeter of the circle when the object of
+			                 circle is passed in the print statement
+
+
+ */
