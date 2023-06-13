d3a4ddc222294e8a5c3833da6c968bf50bab63be
Umran Ozman <umonal@yahoo.com>
2023-06-13 15:51:56 -0400

    TestCircleObjects Class updated

--- src/day27_accessModifiers/TestCircleObjects.java
+++ src/day27_accessModifiers/TestCircleObjects.java
@@ -6,6 +6,16 @@ public class TestCircleObjects {
         Circle circle2 = new Circle(5);
         Circle circle3 = new Circle(7);
 
+        System.out.println(circle1);
+        System.out.println(circle2);
+        System.out.println(circle3);
+
+//        System.out.println(circle1.pi);                   //All these 3 lines will print the same value:3.14
+//        System.out.println(circle2.pi);
+//        System.out.println(circle3.pi);                   //statics can be called from an object but not preferred
+
+        System.out.println(Circle.pi);                      //proper way to call a static is from te Class name
+
 
     }
 }
