a2663df444e4ab787cfa1245969e98606a2f5463
Umran Ozman <umonal@yahoo.com>
2023-06-13 15:39:14 -0400

    Circle Class updated

--- src/day27_accessModifiers/Circle.java
+++ src/day27_accessModifiers/Circle.java
@@ -5,28 +5,32 @@ public class Circle {
     public double radius;
     public double diameter;
 
-    public Circle(double radius){
+    public Circle(double radius){                           //Less the argument needed better it is
         this.radius=radius;
         diameter=2*radius;
     }
 
     /*
-       public static double calcArea(){                      // static only accepts statics
-           return radius * radius * pi;
+       public static double calcArea(){                      // static only accepts statics so we can't use instance
+           return radius * radius * pi;                      // fields & instance methods. Here only the pi is accepted
        }
        */
 
     public double calcArea(){
        return radius*radius*pi;
     }
+
     public double calcPerimeter(){
         return 2*radius*pi;
     }
+
     public static void printPiValue(){
         System.out.println("PI value is: "+pi);
     }
+
     public String toString(){
-        return "Circle{radius="+radius+", diameter="+diameter+", pi="+pi+", area="+calcArea()+", perimeter="+calcPerimeter()+'}';
+        return "Circle{radius="+radius+", diameter="+diameter+", pi="+pi+", area="+calcArea()
+                +", perimeter="+calcPerimeter()+'}';
     }
 
 }
