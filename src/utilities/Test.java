15d60d50514e16249dd348e51d82a6ee141c9395
Umran Ozman <umonal@yahoo.com>
2023-06-13 16:58:26 -0400

    Test Class /utilities package updated

--- src/utilities/Test.java
+++ src/utilities/Test.java
@@ -1,4 +1,16 @@
 package utilities;
+import day27_accessModifiers.Data;
 
 public class Test {
+    public static void main(String[] args){
+
+        System.out.println(Data.d);                     //With regular import we can use the statics
+        System.out.println(Data.e);
+        System.out.println(Data.f);
+
+        Data.method3();
+        Data.method4();
+
+
+    }
 }
