a63096f45eeb2554613103abf63e038ed697ce8a
Umran Ozman <umonal@yahoo.com>
2023-06-13 13:10:25 -0400

    TestCircle Class created

---
+++ src/day27_accessModifiers/TestCircleObjects.java
@@ -0,0 +1,11 @@
+package day27_accessModifiers;
+
+public class TestCircleObjects {
+    public static void main(String[] args) {
+        Circle circle1 = new Circle(3.5);
+        Circle circle2 = new Circle(5);
+        Circle circle3 = new Circle(7);
+
+
+    }
+}
