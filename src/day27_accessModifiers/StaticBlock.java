a8a7b49ecfc30c3c2d743d552828c1bf7ef9a764
Umran Ozman <umonal@yahoo.com>
2023-06-14 10:15:45 -0400

    StaticBlock Class updated

--- src/day27_accessModifiers/StaticBlock.java
+++ src/day27_accessModifiers/StaticBlock.java
@@ -1,4 +1,21 @@
 package day27_accessModifiers;
 
 public class StaticBlock {
+
+    public StaticBlock(){
+        System.out.println("Constructor");              //constructor's execution depend on the object creation
+    }
+//----------------------------------------------------------------------------------------------------------------------
+
+    public static void main(String[] args) {
+
+        System.out.println("Main Method");
+
+        new StaticBlock();
+        new StaticBlock();
+        new StaticBlock();                              //static block's execution doesn't depend on the object creation
+    }
+    static {
+        System.out.println("Static Block");             //static block is always the 1st to run & it only runs once
+    }
 }
