bc442fa5edeb42b8d963ca7f6877e2393098b862
Umran Ozman <umonal@yahoo.com>
2023-06-14 12:02:52 -0400

    StaticInitializationBlock Class updated

--- src/day27_accessModifiers/StaticInitializationBlock.java
+++ src/day27_accessModifiers/StaticInitializationBlock.java
@@ -8,10 +8,18 @@ public class StaticInitializationBlock {
     //public static ExcelSheet sheet;  -----------------We need static blocks to initialize when we have variables which
                                                        //need multiple steps to set them.
 
-    static{                                           //This is an example to show how the static block can be used to
-        a=100;                                        //initialize the static variables.
-        b=20.5;
+//    static{                                           //This is an example to show how the static block can be used to
+//        a=100;                                        //initialize the static variables.
+//        b=20.5;
+//        c="String";
+//    }
+
+    public static void main(){
+        a=100;                                        //Is it a good idea to initialize the statics in other method
+        b=20.5;                                       //blocks like the main()?
         c="String";
     }
+
+
 
 }
