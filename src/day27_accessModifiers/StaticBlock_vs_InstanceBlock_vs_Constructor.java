83e37ad583eb11a3e5143be8443dba4e09094ca2
Umran Ozman <umonal@yahoo.com>
2023-06-14 19:21:03 -0400

    StaticBlock_vs_InstanceBlock_vs_Constructor Class created

--- src/day27_accessModifiers/StaticBlock_vs_InstanceBlock_vs_Constructor.java
+++ src/day27_accessModifiers/StaticBlock_vs_InstanceBlock_vs_Constructor.java
@@ -1,4 +1,26 @@
 package day27_accessModifiers;
 
 public class StaticBlock_vs_InstanceBlock_vs_Constructor {
+
+    public StaticBlock_vs_InstanceBlock_vs_Constructor(){           //Constructor
+        System.out.println("Constructor");
+    }
+//-----------------------------------------------------------------------------------------
+    {                                                              //Instance Block
+        System.out.println("Instance Block");
+            }
+
+//-----------------------------------------------------------------------------------------
+    static{                                                        //Static Block
+        System.out.println("Static Block");
+        }
+
+//-----------------------------------------------------------------------------------------
+    public static void main(String[] args) {                       //main method
+
+        System.out.println("Main Method");
+
+        new StaticBlock_vs_InstanceBlock_vs_Constructor();
+        new StaticBlock_vs_InstanceBlock_vs_Constructor();
+    }
 }
