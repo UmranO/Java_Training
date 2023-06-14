85500d0a682b760b8543fe334ca290aaf379f43e
Umran Ozman <umonal@yahoo.com>
2023-06-14 16:22:02 -0400

    InstanceBlock Class updated

--- src/day27_accessModifiers/InstanceBlock.java
+++ src/day27_accessModifiers/InstanceBlock.java
@@ -1,4 +1,21 @@
 package day27_accessModifiers;
 
 public class InstanceBlock {
+
+    {                                               //Instance Block gets executed everytime when an object is created
+        System.out.println("Instance block");       //just before the constructor.
+    }
+
+    public InstanceBlock(){
+        System.out.println("Constructor");
+    }
+    //-------------------------------------------------
+
+    public static void main(String[] args){
+
+       new InstanceBlock();                         //Instance Block'll be executed after each object creation before
+       new InstanceBlock();                         //the constructor runs
+       new InstanceBlock();
+    }
+
 }
