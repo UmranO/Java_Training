92b4aed4a100a6cfa59c1ea4ab12b5715f240c7c
Umran Ozman <umonal@yahoo.com>
2023-06-14 20:43:07 -0400

    Car Class updated

--- src/day27_accessModifiers/Car.java
+++ src/day27_accessModifiers/Car.java
@@ -1,4 +1,21 @@
 package day27_accessModifiers;
 
-public class Car {
+public class Car {                                                  //outer Class
+
+    public String make, model, color;
+    public int year;
+    public double price;
+
+    public static int wheels = 4;
+
+    public class CarEngine{                                       //inner Class
+
+        public void method(){                                     //if the inner class is not static it can access all
+                                                                  //the members of the outer Class
+            System.out.println(make);                             //either instance variable of the outer Class OR
+            System.out.println(wheels);                           //either static variable of the outer Class
+
+        }
+
+    }
 }
