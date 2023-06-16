1feff8e98f22528a7d7146ea614333b3f6d69d66
Umran Ozman <umonal@yahoo.com>
2023-06-16 10:09:59 -0400

    Test Class /day27... package updated

--- src/day27_accessModifiers/Test.java
+++ src/day27_accessModifiers/Test.java
@@ -9,23 +9,30 @@ public class Test {
         System.out.println(StaticInitializationBlock.b);
         System.out.println(StaticInitializationBlock.c);
 
-//----Asagisi Test for AccessModifiers Class For same package ---------------------------------------------------------
+//----Asagisi Test for AccessModifiers Class / Variables For same package ----------------------------------------------
 
-        System.out.println(AccessModifiers.publicDate);
+        System.out.println(AccessModifiers.publicData);           //public is accessible within same package
+        System.out.println(AccessModifiers.protectedData);        //protected is accessible within same package
+        System.out.println(AccessModifiers.defaultData);          //default is accessible within same package
+        //System.out.println(AccessModifiers.privateData);        //private is not accessible within same package
+
+//----Asagisi Test for AccessModifiers Class / Constructors For same package -------------------------------------------
+
+        new AccessModifiers();                                     //public constructor accessible in the same package
+                                                                   //protected constructor accessible in the same package
+                                                                   //default constructor accessible in the same package
+                                                                   //private not accessible in the same package
+
+//----Asagisi Test for AccessModifiers Class / Methods For same package ------------------------------------------------
+
+        AccessModifiers.publicMethod();                            //All accessible other than the private in the same p.
+        AccessModifiers.protectedMethod();
+        AccessModifiers.defaultMethod();
+       // AccessModifiers.privateMethod();                          //private not accessible in the same package
 
 
 
-
-
-
-
-
-
-
-
-
-
-//----Asagisi benim Java Short'daki ornegi test etmek icin yaptigim bolum /ders degil-------------------------------------
+//----Asagisi benim Java Short'daki ornegi test etmek icin yaptigim bolum /ders degil-----------------------------------
 
        //System.out.println(AccessModifiers.n1);      Even the AccessModifiers Class in the same package can Not call
                                                       //n1 bec. it is private
