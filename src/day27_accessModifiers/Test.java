8f8342cfcd67a286d1467fd3689d705e993ef5c1
Umran Ozman <umonal@yahoo.com>
2023-06-15 20:20:10 -0400

    Test Class updated

--- src/day27_accessModifiers/Test.java
+++ src/day27_accessModifiers/Test.java
@@ -9,5 +9,35 @@ public class Test {
         System.out.println(StaticInitializationBlock.b);
         System.out.println(StaticInitializationBlock.c);
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//----Asagisi benim Java Short'daki ornegi test etmek icin yaptigim bolum /ders degil-------------------------------------
+
+       //System.out.println(AccessModifiers.n1);      Even the AccessModifiers Class in the same package can Not call
+                                                      //n1 bec. it is private
+        System.out.println(AccessModifiers.n2);       //n2 has default modifier so can be called in the same package
+
+        System.out.println(AccessModifiers.n3);       //n3 has public modifier so can be called in the same package
+
+
+       //AccessModifiers.method1();                   method1 can Not be called bec. it has private modifier
+
+        AccessModifiers.method2();                    //method2 has default modifier so canbe called in the same package
+
+        AccessModifiers.method3();                   //method3 has default modifier so can be called in the same package
+
+
     }
 }
