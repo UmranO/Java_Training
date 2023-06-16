5eb9eb5656f7611df9dae060e3481ec6b4b4a71d
Umran Ozman <umonal@yahoo.com>
2023-06-16 10:09:07 -0400

    AccessModifiers updated

--- src/day27_accessModifiers/AccessModifiers.java
+++ src/day27_accessModifiers/AccessModifiers.java
@@ -1,19 +1,69 @@
 package day27_accessModifiers;
 
 public class AccessModifiers {
+//----Variables--------------------------------------------------------------------------------------------------------
+public static int publicData=200;
+protected static int protectedData=300;
+static int defaultData=400;
+private static int privateData=500;
+//----Constructor-------------------------------------------------------------------------------------------------------
 
-public static int publicDate=200;
+//public AccessModifiers(){                             //Constructor with public access modifier
+// }
+//-----------------------
+// protected AccessModifiers(){                         //Constructor with protected access modifier
+// }
+//-----------------------
+//    AccessModifiers(){                                //Constructor with default access modifier
+//    }
+//-----------------------
+//private AccessModifiers(){
+//}
+
+//----Method--------------------------------------------------------------------------------------
+
+    public static void publicMethod(){
+        System.out.println("Public");
+    }
+    //--------------------
+    protected static void protectedMethod(){
+        System.out.println("Protected");
+    }
+    //--------------------
+    static void defaultMethod(){
+        System.out.println("Default");
+    }
+    //--------------------
+    private static void privateMethod(){
+        System.out.println("Private");
+    }
 
 
-public static void main(String[] args){
+    public static void main(String[] args){
 
-    System.out.println(publicDate);                     //public data can be accessed in the same class
-                                                        //same package test:Test different package test:utilities/Test
+    System.out.println(publicData);                     //public datails accessible within the same class
+                                                        //same package testini Test Class'da yaptim
+                                                        //different package test:utilities/Test Class'da yaptim
 
+    System.out.println(protectedData);                  //protected is accessible within  the same Class
+                                                        //other ()s within the same class has access to protected
 
+    System.out.println(defaultData);                    //default is accessible within  the same Class
+
+    System.out.println(privateData);                    //private is accessible within  the same Class
+
+        System.out.println("-----------------------------");
+
+    new AccessModifiers();                              //public constructor accesible in the same class
+
+        System.out.println("-----------------------------");
+
+        publicMethod();                                 //All the methods are accessible in the same Class
+        protectedMethod();
+        defaultMethod();
+        privateMethod();
 
 }
-
 
 
 //Asagisi JavaShort icin yaptim Sinifta yapilmadi. Tested in Test3ForAccessModifiers Class under Utilities
@@ -24,11 +74,12 @@ public static void main(String[] args){
     public static int n3;               //public modifier
 
 
-//----Methods--------------------------------------------------------------------------------------------------------
+//----Methods-----------------------------------------------------------------------------------------------------------
 
     private static void method1(){ System.out.println("method with private access modifier");}       //private modifier
     static void method2(){System.out.println("method with default access modifier");}                //default modifier
     public static void method3() { System.out.println("method with public access modifier");}        //public modifier
+
 
 
 }
