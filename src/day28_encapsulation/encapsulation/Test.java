576adfd8ecaf64e096b479c28fa6b34b27b47984
Umran Ozman <umonal@yahoo.com>
2023-06-17 13:25:40 -0400

    Test Class created

--- src/day28_encapsulation/encapsulation/Test.java
+++ src/day28_encapsulation/encapsulation/Test.java
@@ -1,4 +1,22 @@
 package day28_encapsulation.encapsulation;
 
 public class Test {
+    public static void main(String[] args) {
+
+        Student student=new Student();
+      //student.age=-200;                   if the access mod. is public in the Custom Class any value can be set which
+                                            //is not good we want control on our data
+
+      //student.age;                        If the instance variable private the only way we can get the age is through
+
+        student.getAge();                              //the getter
+        System.out.println(student.getAge());          //=>0 bec. it is the default value
+
+        student.setAge(-28);                            //28 is the argument & it will be assigned to this parameter age
+        System.out.println(student.getAge());          //=>Now it will print 28 bec. we set it in line15
+
+        System.out.println("Test Completed");
+
+
+    }
 }
