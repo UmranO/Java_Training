c6d39c0c289ec2239c1f9931473c4109cff018d3
Umran Ozman <umonal@yahoo.com>
2023-06-17 12:14:32 -0400

    Student Class created

--- src/day28_encapsulation/encapsulation/Student.java
+++ src/day28_encapsulation/encapsulation/Student.java
@@ -1,4 +1,22 @@
 package day28_encapsulation.encapsulation;
 
 public class Student {
+
+  //  public int age;
+    private int age;
+
+  //getter
+    public int getAge(){
+      return age;
+    }
+
+  //setter                                  Whatever argument passed to setAge() in the Test Class will be assigned to
+    public void setAge(int age){            //the parameter age
+      this.age=age;                         //then it will be assigned to the instance variable age
+    }
+
+
+
+
 }
+
