9ec7345e6ca514f52cec87c54366a5e485612635
Umran Ozman <umonal@yahoo.com>
2023-06-17 14:30:25 -0400

    Test Class updated

--- src/day28_encapsulation/encapsulation/Test.java
+++ src/day28_encapsulation/encapsulation/Test.java
@@ -8,15 +8,18 @@ public class Test {
                                             //is not good we want control on our data
 
       //student.age;                        If the instance variable private the only way we can get the age is through
+                                            //the getter
 
-        student.getAge();                              //the getter
+        student.getAge();
         System.out.println(student.getAge());          //=>0 bec. it is the default value
 
-        student.setAge(-28);                            //28 is the argument & it will be assigned to this parameter age
+        student.setAge(28);                            //28 is the argument & it will be assigned to this parameter age
         System.out.println(student.getAge());          //=>Now it will print 28 bec. we set it in line15
 
         System.out.println("Test Completed");
 
+        student.setName("Ali9");                       //"Invalid name: Ali9"
 
+        System.out.println("Test completed");         //If No problem it should complete codes above and print this
     }
 }
