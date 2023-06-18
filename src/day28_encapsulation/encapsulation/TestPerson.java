8209cfe7d311ad9ab5e1e16cddfa6aeefc65f345
Umran Ozman <umonal@yahoo.com>
2023-06-17 23:53:35 -0400

    TestPerson Class updated

--- src/day28_encapsulation/encapsulation/TestPerson.java
+++ src/day28_encapsulation/encapsulation/TestPerson.java
@@ -1,4 +1,16 @@
 package day28_encapsulation.encapsulation;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class TestPerson {
+    public static void main(String[] args) {
+
+        Person p1=new Person("Umran",52,'M', LocalDate.of(1970,2,13));
+        System.out.println(p1);
+
+        System.out.println(p1.getName());
+        DateTimeFormatter df= DateTimeFormatter.ofPattern("dd/MM/Y");
+        System.out.println(p1.getDateOfBirth().format(df));
+    }
 }
