4d33c1c8c898f15017b1a4d6873cab06251f66ff
Umran Ozman <umonal@yahoo.com>
2023-06-17 22:47:33 -0400

    TestEmployee Class updated

--- src/day28_encapsulation/encapsulation/TestEmployee.java
+++ src/day28_encapsulation/encapsulation/TestEmployee.java
@@ -1,4 +1,14 @@
 package day28_encapsulation.encapsulation;
 
+import java.nio.charset.StandardCharsets;
+
 public class TestEmployee {
+    public static void main(String[] args) {
+        Employee employee1=new Employee();
+        employee1.setSalary(100);
+        System.out.println(employee1.getSalary());
+
+        employee1.setName("      ");                           //Since the arg given only contains space -so it is blank
+        System.out.println(employee1.getName());
+    }
 }
