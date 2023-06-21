7ca563fd97d531813056f7ec8a6f2f3ba5401617
Umran Ozman <umonal@yahoo.com>
2023-06-20 22:34:33 -0400

    Tiger class updated

--- src/day29_inheritance/animalTask/Tiger.java
+++ src/day29_inheritance/animalTask/Tiger.java
@@ -1,4 +1,10 @@
 package day29_inheritance.animalTask;
 
-public class Tiger {
+public class Tiger extends Animal{ // Tiger Is An Animal
+
+
+    public void hunt(){
+        System.out.println(getName()+" is hunting");
+    }
+
 }
