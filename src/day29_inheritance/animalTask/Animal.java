907cdae683f9b0b0a8ceb583d790c4fb8a156a68
Umran Ozman <umonal@yahoo.com>
2023-06-20 21:41:42 -0400

    Animal class created

--- src/day29_inheritance/animalTask/Animal.java
+++ src/day29_inheritance/animalTask/Animal.java
@@ -1,4 +1,11 @@
 package day29_inheritance.animalTask;
 
-public class Animal {
+public class Animal {                                        //is the Parent class of Dog, Cat, Tiger
+
 }
+
+/*
+            Animal Class
+			attributes:name, breed, gender, size, color, age
+			methods:setInfo(), eat(), sleep(), drink(), toString()
+ */
