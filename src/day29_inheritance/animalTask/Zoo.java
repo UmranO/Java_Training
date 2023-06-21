9b8c800ec673828e80707112a24b2079115ae2b9
Umran Ozman <umonal@yahoo.com>
2023-06-20 23:39:44 -0400

    Zoo class updated

--- src/day29_inheritance/animalTask/Zoo.java
+++ src/day29_inheritance/animalTask/Zoo.java
@@ -1,4 +1,22 @@
 package day29_inheritance.animalTask;
 
 public class Zoo {
+
+    public static void main(String[] args) {
+
+        Dog dog = new Dog();
+        dog.setInfo("Max", "Husky", 'M', 4, "Large", "White");
+
+        Cat cat = new Cat();
+        cat.setInfo("Felicia", "stray", 'F', 3, "Medium", "Black and White");
+
+        Tiger tiger = new Tiger();
+        tiger.setInfo("Sher Khan", "Bengal", 'M', 5, "Large", "Orange");
+
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(tiger);
+
+    }
+
 }
