1ff3cf5cf0e08fccae81326e519d0e902e0cf0d7
Umran Ozman <umonal@yahoo.com>
2023-06-23 19:53:02 -0400

    ProtectedAccessModifier Class updated

--- src/day31_inheritance/ProtectedAccessModifier.java
+++ src/day31_inheritance/ProtectedAccessModifier.java
@@ -2,7 +2,10 @@ package day31_inheritance;
 
 public class ProtectedAccessModifier {
 
-    protected static int a=100;
+        protected static int a = 100;
 
-    protected static void method1(){}
-}
+        protected static void methodA(){
+
+        }
+
+    }
