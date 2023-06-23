e8705053b85df1b8ee5a5ad301515b978bcacdff
Umran Ozman <umonal@yahoo.com>
2023-06-23 14:05:48 -0400

    Audi Class updated

--- src/day31_inheritance/vehicleTask/Audi.java
+++ src/day31_inheritance/vehicleTask/Audi.java
@@ -1,4 +1,9 @@
 package day31_inheritance.vehicleTask;
 
-public class Audi {
+public class Audi extends Car{
+
+    public Audi(String brand, String model, String color, int year, double price) {
+        super(brand, model, color, year, price);
+    }
+
 }
