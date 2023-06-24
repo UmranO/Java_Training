f048e4ebefc54b7b888658870e334d64919c3d81
Umran Ozman <umonal@yahoo.com>
2023-06-24 18:22:25 -0400

    TestShapeObjects Class updated

--- src/day31_inheritance/shape_methodOverriding/TestShapeObjects.java
+++ src/day31_inheritance/shape_methodOverriding/TestShapeObjects.java
@@ -5,18 +5,25 @@ public class TestShapeObjects {
     public static void main(String[] args) {
 
         Square square = new Square(5);
-
         System.out.println(square);
-
         square.draw();
 
-        System.out.println("----------------------------");
+        System.out.println("-------------------------------------------------------");
 
         Circle circle = new Circle(5.0);
-
         System.out.println(circle);
-
         circle.draw();
+
+        System.out.println("-------------------------------------------------------");
+
+        Rectangle rec1=new Rectangle(3.0,5.0);
+        System.out.println(rec1);
+        System.out.println(rec1.area());
+        System.out.println(rec1.perimeter());
+        rec1.draw();
+
+        System.out.println("-------------------------------------------------------");
+
 
     }
 }
