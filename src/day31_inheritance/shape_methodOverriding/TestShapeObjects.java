1b2919af7f142fc51a91ef858412363628152f53
Umran Ozman <umonal@yahoo.com>
2023-06-24 18:10:54 -0400

    TestShapeObjects Class created

--- src/day31_inheritance/shape_methodOverriding/TestShapeObjects.java
+++ src/day31_inheritance/shape_methodOverriding/TestShapeObjects.java
@@ -1,4 +1,22 @@
 package day31_inheritance.shape_methodOverriding;
 
 public class TestShapeObjects {
+
+    public static void main(String[] args) {
+
+        Square square = new Square(5);
+
+        System.out.println(square);
+
+        square.draw();
+
+        System.out.println("----------------------------");
+
+        Circle circle = new Circle(5.0);
+
+        System.out.println(circle);
+
+        circle.draw();
+
+    }
 }
