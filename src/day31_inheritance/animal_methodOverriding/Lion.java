6a9656b47613af7d86ccacc548eb45a7e928e715
Umran Ozman <umonal@yahoo.com>
2023-06-23 22:14:13 -0400

    Lion Class updated

--- src/day31_inheritance/animal_methodOverriding/Lion.java
+++ src/day31_inheritance/animal_methodOverriding/Lion.java
@@ -1,4 +1,29 @@
 package day31_inheritance.animal_methodOverriding;
 
-public class Lion {
+public class Lion extends Animal {
+
+    private boolean isAfricanLion;
+
+    public Lion(String name, String breed, char gender, int age, String size, String color, boolean isAfricanLion) {
+        super(name, breed, gender, age, size, color);
+        setAfricanLion(isAfricanLion);
+    }
+    public boolean isAfricanLion() {
+        return isAfricanLion;
+    }
+
+    public void setAfricanLion(boolean africanLion) {
+        isAfricanLion = africanLion;
+    }
+
+//    @Override                                 This is the output of the shortcut of Intellij to override a ()
+//    public void eat() {
+//        super.eat();                          This is calling the parent class's eat() it will give the same output
+//   }                                          so we need to override to have the Lion's implementation
+    @Override
+    public void eat() {
+        System.out.println("Lion "+getName()+" is eating deer");
+    }
+
+
 }
