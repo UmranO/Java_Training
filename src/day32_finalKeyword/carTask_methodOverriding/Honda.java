3004f6cbee166009f70b4af9d3db80b62efb97d8
Umran Ozman <umonal@yahoo.com>
2023-06-26 14:19:36 -0400

    Honda class updated

--- src/day32_finalKeyword/carTask_methodOverriding/Honda.java
+++ src/day32_finalKeyword/carTask_methodOverriding/Honda.java
@@ -1,4 +1,16 @@
 package day32_finalKeyword.carTask_methodOverriding;
 
-public class Honda {
+public class Honda extends  Car{
+    public Honda(String model, String color, int year, double price) {
+        super(model, color, year, price);
+    }
 }
+/*
+2. Create the following sub classes and override the start() method if needed:
+	Car Class start():  public void start(){
+                        System.out.println("Press the brake and twist the key to ignition");
+                         }
+    Honda start(): "Press the brake and twist the key to ignition"
+
+
+ */
