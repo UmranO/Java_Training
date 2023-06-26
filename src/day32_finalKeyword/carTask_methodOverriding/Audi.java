32a2489d750d4e4fd80488a1b41278ebbc43216d
Umran Ozman <umonal@yahoo.com>
2023-06-26 14:20:54 -0400

    Audi class updated

--- src/day32_finalKeyword/carTask_methodOverriding/Audi.java
+++ src/day32_finalKeyword/carTask_methodOverriding/Audi.java
@@ -1,4 +1,24 @@
 package day32_finalKeyword.carTask_methodOverriding;
 
-public class Audi {
-}
+public class Audi extends Car {
+
+        public Audi(String model, String color, int year, double price) {
+            super(model, color, year, price);
+        }
+
+        @Override
+        public void start(){
+            System.out.println("Press the start button to start "+getMake()+" "+getModel());
+        }
+
+    }
+/*
+2. Create the following sub classes and override the start() method if needed:
+	Car Class start():  public void start(){
+                        System.out.println("Press the brake and twist the key to ignition");
+                         }
+
+    Audi  start(): "Press the start button"
+
+
+ */
