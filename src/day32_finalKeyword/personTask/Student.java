9041d856c5781b020e1e82bf1d775cb2a8f7a9a3
Umran Ozman <umonal@yahoo.com>
2023-06-27 15:47:36 -0400

    Student class updated

--- src/day32_finalKeyword/personTask/Student.java
+++ src/day32_finalKeyword/personTask/Student.java
@@ -1,4 +1,28 @@
 package day32_finalKeyword.personTask;
 
-public class Student {
+import java.time.LocalDate;
+
+public class Student extends Person {
+
+    private char grade;
+
+    public Student(String name, char gender, LocalDate dateOfBirth, char grade) {
+        super(name, gender, dateOfBirth);
+        setGrade(grade);
+    }
+
+    public char getGrade() {
+        return grade;
+    }
+
+    public void setGrade(char grade) {
+        this.grade = grade;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString().replace("Person","Student").replace("}",", ")+
+                "grade=" + grade +
+                '}';
+    }
 }
