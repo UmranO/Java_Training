60bc4a6ba50db06fd51df678db44e78d8fa72f77
Umran Ozman <umonal@yahoo.com>
2023-06-29 14:09:32 -0400

    Teacher class updated

--- src/day33_abstraction/employeeTask/Teacher.java
+++ src/day33_abstraction/employeeTask/Teacher.java
@@ -1,4 +1,29 @@
 package day33_abstraction.employeeTask;
 
-public class Teacher {
+public class Teacher extends Employee{
+
+    private String programmingLanguage;
+
+    public Teacher(String name, int age, char gender, String id, String jobTitle, double salary, String programmingLanguage) {
+        super(name, age, gender, id, jobTitle, salary);
+        setProgrammingLanguage(programmingLanguage);
+    }
+
+    public String getProgrammingLanguage() {
+        return programmingLanguage;
+    }
+
+    public void setProgrammingLanguage(String programmingLanguage) {
+        this.programmingLanguage = programmingLanguage;
+    }
+    public void work(){
+        System.out.println(getName()+" is coding in "+programmingLanguage);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString().replace("}","programmingLanguage='" + programmingLanguage + '\'' +
+                '}');
+
+    }
 }
