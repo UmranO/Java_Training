ba4f3d3e2e60613e6f640913262bd0a84e8048d5
Umran Ozman <umonal@yahoo.com>
2023-06-29 14:16:02 -0400

    Teacher class updated/Previously I put Developer to Teacher by mistake

--- src/day33_abstraction/employeeTask/Teacher.java
+++ src/day33_abstraction/employeeTask/Teacher.java
@@ -1,29 +1,14 @@
 package day33_abstraction.employeeTask;
 
-public class Teacher extends Employee{
+public class Teacher extends Employee {
 
-    private String programmingLanguage;
+        public Teacher(String name, int age, char gender, String id, String jobTitle, double salary) {
+            super(name, age, gender, id, jobTitle, salary);
+        }
 
-    public Teacher(String name, int age, char gender, String id, String jobTitle, double salary, String programmingLanguage) {
-        super(name, age, gender, id, jobTitle, salary);
-        setProgrammingLanguage(programmingLanguage);
-    }
-
-    public String getProgrammingLanguage() {
-        return programmingLanguage;
-    }
-
-    public void setProgrammingLanguage(String programmingLanguage) {
-        this.programmingLanguage = programmingLanguage;
-    }
-    public void work(){
-        System.out.println(getName()+" is coding in "+programmingLanguage);
-    }
-
-    @Override
-    public String toString() {
-        return super.toString().replace("}","programmingLanguage='" + programmingLanguage + '\'' +
-                '}');
+        @Override
+        public void work() {
+            System.out.println(getName() + " is teaching");
+        }
 
     }
-}
