362464057033b90284ce6b8ebe7cd2f131a57a63
Umran Ozman <umonal@yahoo.com>
2023-06-29 20:35:26 -0400

    Tesla class updated

--- src/day34_abstraction/carTask/Tesla.java
+++ src/day34_abstraction/carTask/Tesla.java
@@ -1,4 +1,23 @@
 package day34_abstraction.carTask;
 
-public class Tesla {
+public class Tesla extends Car {
+
+    public Tesla(String model,String color, int year, double price){
+        super(model,color, year,price);
+    }
+
+    public void start(){
+        System.out.println("Just say start my "+getColor()+" "+getModel());
+    }
+    public void autoPilot(){
+        System.out.println("To have an autopilot first you have to have "+getMake()+" "+getModel());
+    }
+
 }
+
+/*
+Create the following sub classes of Car
+
+			Tesla extra methods:autoPilot()
+
+ */
