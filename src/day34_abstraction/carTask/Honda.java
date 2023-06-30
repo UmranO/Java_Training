b7d82b64b01e08f6dcdfbd62a69f12ccbc3170a9
Umran Ozman <umonal@yahoo.com>
2023-06-29 20:13:46 -0400

    Honda class updated

--- src/day34_abstraction/carTask/Honda.java
+++ src/day34_abstraction/carTask/Honda.java
@@ -1,4 +1,12 @@
 package day34_abstraction.carTask;
 
-public class Honda {
+public class Honda extends Car{
+    public Honda(String model, String color, int year, double price){
+        super(model,color,year,price);
+    }
+@Override
+    public void start(){
+        System.out.println("To start "+ getMake() +" car you have to turn the ignition.");
+    }
+
 }
