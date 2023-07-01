5d14d5b883f1d7e0f9ca32fe820670729fe0c6d6
Umran Ozman <umonal@yahoo.com>
2023-07-01 12:54:14 -0400

    Dog class updated

--- src/day34_abstraction/animalTask/Dog.java
+++ src/day34_abstraction/animalTask/Dog.java
@@ -1,6 +1,6 @@
 package day34_abstraction.animalTask;
 
-public class Dog extends Animal implements Playable {
+public class Dog extends Animal implements Playable, Swimmable{
 
     public Dog(String name, String breed, char gender, int age, String size, String color) {
         super(name, breed, gender, age, size, color);
@@ -11,4 +11,13 @@ public class Dog extends Animal implements Playable {
         System.out.println("Dog "+ getName() +" is eating dog food");
     }
 
+    public void play(){
+        System.out.println(getSize()+ " "+getBreed()+ " of the dogs determine if the dod likes to play.");
+    }
+
+    public void swim(){
+        System.out.println("All the dogs like swimming");
+    }
+
+    public void bark(){  System.out.println(getName() +" is barking");}
 }
