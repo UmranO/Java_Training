d92f7dc9a36c3340d5a47ea2a9453772e6d41ff6
Umran Ozman <umonal@yahoo.com>
2023-07-01 12:50:14 -0400

    Cat class updated

--- src/day34_abstraction/animalTask/Cat.java
+++ src/day34_abstraction/animalTask/Cat.java
@@ -1,6 +1,6 @@
 package day34_abstraction.animalTask;
 
-public class Cat extends Animal{
+public class Cat extends Animal implements Playable{
 
     public Cat(String name, String breed, char gender, int age, String size, String color) {
         super(name, breed, gender, age, size, color);
@@ -9,5 +9,8 @@ public class Cat extends Animal{
         System.out.println("Cat "+getName()+" is eating cat food");
     }
 
+    public void play(){
+        System.out.println(getBreed()+" cats are the ones that like to play most.");
+    }
 
 }
