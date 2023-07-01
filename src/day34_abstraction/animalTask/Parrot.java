e7e1afa0e4e4f34cd035afc4149aabae52e4b558
Umran Ozman <umonal@yahoo.com>
2023-07-01 13:11:07 -0400

    Parrot class updated

--- src/day34_abstraction/animalTask/Parrot.java
+++ src/day34_abstraction/animalTask/Parrot.java
@@ -1,6 +1,6 @@
 package day34_abstraction.animalTask;
 
-public class Parrot extends Animal{
+public class Parrot extends Animal implements Playable, Flyable{
     public Parrot(String name, String breed, char gender, int age, String size, String color) {
         super(name, breed, gender, age, size, color);
     }
@@ -8,5 +8,11 @@ public class Parrot extends Animal{
     @Override
     public void eat() {
         System.out.println("Parrot "+getName()+" is eating seeds");
+    }
+    public void play(){
+        System.out.println(getName()+" is a "+getBreed()+" which loves playing");
+    }
+    public void fly(){
+        System.out.println(getGender()+ " is not a determining factor for how fast a bird can fly");
     }
 }
