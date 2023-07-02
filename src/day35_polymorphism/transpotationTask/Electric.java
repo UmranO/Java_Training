a768b933145e6037ea4ef14a2a2da28d7e92b4a6
Umran Ozman <umonal@yahoo.com>
2023-07-02 13:18:35 -0400

    Electric interface updated

--- src/day35_polymorphism/transpotationTask/Electric.java
+++ src/day35_polymorphism/transpotationTask/Electric.java
@@ -1,4 +1,14 @@
 package day35_polymorphism.transpotationTask;
 
 public interface Electric {
+    void charge();
 }
+
+/*
+	4. Create an interface named Electric
+
+				abstract methods:
+					charge()
+
+
+ */
