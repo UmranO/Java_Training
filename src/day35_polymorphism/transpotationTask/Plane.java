75dd4f289473365241df4ee537aafd22db0b00f6
Umran Ozman <umonal@yahoo.com>
2023-07-02 13:13:28 -0400

    Plane abstract class updated

--- src/day35_polymorphism/transpotationTask/Plane.java
+++ src/day35_polymorphism/transpotationTask/Plane.java
@@ -1,4 +1,27 @@
 package day35_polymorphism.transpotationTask;
+import day34_abstraction.animalTask.Flyable;
 
-public class Plane {
+public abstract class Plane extends Transportation implements Flyable {
+
+    public Plane(String make, String model, String color, int year, double price) {
+        super(make, model, color, year, price);
+    }
+
+
+    public void land() {
+        System.out.println("Plane " + getMake() + " " + getModel() + " is landing");
+    }
+
+    @Override
+    public void fly() {
+        System.out.println(getMake() + " " + getModel() + " is flying");
+    }
 }
+/*
+6. Create an abstract sub class of Transpotation named Plane that implements Flyable
+
+				non-abstract method:
+					land()
+
+
+ */
