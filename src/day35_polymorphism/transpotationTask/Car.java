e9257469856244ef4ec61444e154d4020500fda4
Umran Ozman <umonal@yahoo.com>
2023-07-02 13:12:00 -0400

    Car abstract class updated

--- src/day35_polymorphism/transpotationTask/Car.java
+++ src/day35_polymorphism/transpotationTask/Car.java
@@ -1,4 +1,23 @@
 package day35_polymorphism.transpotationTask;
 
-public class Car {
+public abstract class Car extends Transportation{
+
+    public Car(String make, String model, String color, int year, double price) {
+        super(make, model, color, year, price);
+    }
+
+    public void drive(){
+        System.out.println("Driving "+getMake()+" "+getModel());
+    }
+
+
 }
+
+/*
+5. Create an abstract sub class of Transpotation named Car
+
+				extra methods:
+					drive()
+
+
+ */
