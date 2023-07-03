02b329da1ee68cafa75da2241f10c7e6f2310f6b
Umran Ozman <umonal@yahoo.com>
2023-07-03 13:25:26 -0400

    AutoPilot interface updated

--- src/day35_polymorphism/transpotationTask/AutoPilot.java
+++ src/day35_polymorphism/transpotationTask/AutoPilot.java
@@ -1,4 +1,17 @@
 package day35_polymorphism.transpotationTask;
 
-public interface AutoPilot {
-}
+    public interface AutoPilot extends AutoPark{
+
+        void selfDrive();                                   //unique method of AutoPilot besides the ones that will be
+                                                            //inherited from AutoPark
+
+    }
+/*
+3. Create a sub interface of AutoPark named AutoPilot:
+
+				abstract methods:
+						autoPilot()
+
+
+
+ */
