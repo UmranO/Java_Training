e39c719ccd057c8ac387edf4a113779ea3f26cd1
Umran Ozman <umonal@yahoo.com>
2023-07-04 16:24:08 -0400

    PolymorphismIntro class updated

--- src/day35_polymorphism/PolymorphismIntro.java
+++ src/day35_polymorphism/PolymorphismIntro.java
@@ -1,5 +1,19 @@
 package day35_polymorphism;
+import day34_abstraction.animalTask.*;
 public class PolymorphismIntro {
+    public static void main(String[] args) {
 
+        Dog dog = new Dog("Max", "Husky", 'M', 4, "Small", "White");
 
+        Cat cat = new Cat("Max", "Husky", 'M', 4, "Small", "White");
+
+        Tiger tiger = null;                            //Bu objectleri create emek yerine kullanabilmek icin null assign
+        Eagle eagle = null;                            //etti.
+        Lion lion = null;
+        Parrot parrot = null;
+        Dolphin dolphin = null;
+        Shark shark = null;
+        Duck duck = null;
+
+    }
 }
