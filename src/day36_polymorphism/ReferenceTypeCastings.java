5913f773a3a4b2a9e1264242a088ff3e4a6c4412
Umran Ozman <umonal@yahoo.com>
2023-07-06 10:30:17 -0400

    ReferenceTypeCastings class updated

--- src/day36_polymorphism/ReferenceTypeCastings.java
+++ src/day36_polymorphism/ReferenceTypeCastings.java
@@ -1,4 +1,19 @@
 package day36_polymorphism;
 
+import day29_inheritance.animalTask.Animal;
+import day29_inheritance.animalTask.Cat;
+import day29_inheritance.animalTask.Dog;
+
 public class ReferenceTypeCastings {
+    public static void main(String[] args) {
+
+        Dog dog=new Dog();                  ///Default reference type of new Dog() is Dog
+      //Cat cat=new Dog();                  //So you can not assign it to another type if it is not the parent type.
+
+        Animal animal=new Dog();            //implicit casting done automatically
+
+        System.out.println("-----------------------------------------------------");
+
+
+    }
 }
