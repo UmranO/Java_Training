e3cfd8a879534dc10b5d317c931bb4636d881f2b
Umran Ozman <umonal@yahoo.com>
2023-07-06 15:08:58 -0400

    Circle class updated

--- src/day36_polymorphism/Circle.java
+++ src/day36_polymorphism/Circle.java
@@ -1,4 +1,22 @@
 package day36_polymorphism;
 
 public class Circle {
+
+        private double radius;
+
+        public final static double PI = 3.14;
+
+        public Circle(double radius) {
+            setRadius(radius);
+        }
+
+        public double getRadius() {
+            return radius;
+        }
+
+        public void setRadius(double radius) {
+            this.radius = radius;
+        }
+
+
 }
