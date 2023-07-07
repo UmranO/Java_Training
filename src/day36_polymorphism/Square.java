87b570d286e89479935d1e7fb01cfa6184e3fc89
Umran Ozman <umonal@yahoo.com>
2023-07-07 17:48:56 -0400

    Square class updated

--- src/day36_polymorphism/Square.java
+++ src/day36_polymorphism/Square.java
@@ -1,4 +1,39 @@
 package day36_polymorphism;
 
 public class Square {
+    private double side;
+
+    public Square(double side){
+        this.side=side;
+    }
+//----M Version---------------------------------------------------------------------------------------------------------
+@Override
+public boolean equals(Object obj) {
+
+    if( !(obj instanceof Square) ){
+        System.err.println("Invalid Object, Object must be square");
+        System.exit(1);
+    }
+
+    if(side ==  ( (Square)obj ).side ){
+        return true;
+    }
+
+    return false;
+}
+
+//----UO Version--------------------------------------------------------------------------------------------------------
+  /*
+    public boolean equals(Object obj){
+        if(obj instanceof Square){
+            if(this.side==(((Square)obj)).side){
+                return true;
+            }
+        }else {
+            System.err.println("Invalid object, not a square");
+            System.exit(1);
+        }return false;
+    }
+
+    */
 }
