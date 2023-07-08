036271803511b2103ca08ce4bea564ab49e250e3
Umran Ozman <umonal@yahoo.com>
2023-07-08 18:28:50 -0400

    ExceptionsIntro class updated

--- src/day37_exceptions/ExceptionsIntro.java
+++ src/day37_exceptions/ExceptionsIntro.java
@@ -5,11 +5,11 @@ public class ExceptionsIntro {
 
             String str = "Java";
 
-            //char ch = str.charAt(250); // unchecked exceptions
+            //char ch = str.charAt(250);                                    unchecked exceptions
             //System.out.println(ch);
 
             Pizza pizza = null;
-            //  pizza.calcCost();  // unchecked exception
+            //  pizza.calcCost();                                           unchecked exception
 
 
             // System.out.println( 50/0 ); // un-checked exception
@@ -18,5 +18,15 @@ public class ExceptionsIntro {
 
             System.out.println("---------------------------------");
 
+        int score = 100;
+
+        if(score > 59){                                                     //A bug
+            System.out.println("Your grade is D");
+        }else if(score > 70){
+            System.out.println("Your grade is C");
+        }
+
+        System.out.println("---------------------------------");
+        
     }
 }
