96f7ae0ceb6a35b2e22b2f71a2041b73d2c27cc6
Umran Ozman <umonal@yahoo.com>
2023-07-08 12:02:40 -0400

    TestPizzaObject class updated

--- src/day37_exceptions/TestPizzaObject.java
+++ src/day37_exceptions/TestPizzaObject.java
@@ -1,14 +1,22 @@
 package day37_exceptions;
 
 public class TestPizzaObject {
-    public static void main(String[] args) {
+    public static void main(String[] args) {                   //2 different Pizza objects are allocated in the Heap now
 
         Pizza pizza1 = new Pizza('S', 2, 3);
         Pizza pizza2 = new Pizza('S', 2, 3);
 
-        System.out.println(pizza1 == pizza2);
+        System.out.println(pizza1 == pizza2);                 //=>false             //checks if they are the same object
 
-        System.out.println(pizza1.equals(pizza2));
+        System.out.println(pizza1.equals(pizza2));            //=>true             //checks if they are equal objects
 
+//----If we pass different objects--------------------------------------------------------------------------------------
+
+        //String object:
+        System.out.println(pizza1.equals("Pizza"));          //=>Invalid object: String
+        //boolean
+        System.out.println(pizza1.equals(true));              //=>Invalid object: true
+        //integer
+        System.out.println(pizza1.equals(100));              //=>Invalid object: 100
     }
 }
