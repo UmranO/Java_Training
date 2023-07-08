02854df63486c5a0bde82b0e30deeaad61af4d91
Umran Ozman <umonal@yahoo.com>
2023-07-08 11:53:45 -0400

    TestPizzaObject class updated

--- src/day37_exceptions/TestPizzaObject.java
+++ src/day37_exceptions/TestPizzaObject.java
@@ -1,4 +1,14 @@
 package day37_exceptions;
 
 public class TestPizzaObject {
+    public static void main(String[] args) {
+
+        Pizza pizza1 = new Pizza('S', 2, 3);
+        Pizza pizza2 = new Pizza('S', 2, 3);
+
+        System.out.println(pizza1 == pizza2);
+
+        System.out.println(pizza1.equals(pizza2));
+
+    }
 }
