a9d120dc52030da99a18d9050f7857a67aa65fc4
Umran Ozman <umonal@yahoo.com>
2023-07-09 19:22:50 -0400

    TryCatchBlocks class updated

--- src/day37_exceptions/TryCatchBlocks.java
+++ src/day37_exceptions/TryCatchBlocks.java
@@ -1,4 +1,20 @@
 package day37_exceptions;
 
 public class TryCatchBlocks {
-}
+    public static void main(String[] args) {
+
+        System.out.println("Program1 started");
+
+        try {
+
+            System.out.println(9 / 0); // Arithmetic Exception
+
+            System.out.println("Try block");
+
+        }catch(ArithmeticException e){
+
+            System.out.println("Catch Block");
+
+        }
+
+    }
