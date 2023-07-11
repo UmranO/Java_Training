4252a7497b4bcd2bfbd02beb0f4584663686354e
Umran Ozman <umonal@yahoo.com>
2023-07-11 17:47:14 -0400

    TryCatchBlocks class updated

--- src/day37_exceptions/TryCatchBlocks.java
+++ src/day37_exceptions/TryCatchBlocks.java
@@ -11,10 +11,27 @@ public class TryCatchBlocks {
 
             System.out.println("Try block");
 
-        }catch(ArithmeticException e){
+        } catch (ArithmeticException e) {
 
             System.out.println("Catch Block");
 
         }
+        System.out.println("Program1 ended");
+
+        System.out.println("-------------------------------------------------------------------");
+
+        System.out.println("Program2 started");
+
+        String str = null;
+
+        try {                                                 //We put the code fragment with the potential exception
+            System.out.println(str.toLowerCase());            //source in the try block. If it is solved exception will
+            System.out.println("Try Block");                  //be gone and no need for the catch block but if not
+        }                                                     //if we give a correct exception class as the reference
+        catch (RuntimeException e){                           //type the exception object will cathch it and handle it.
+            System.out.println("Catch Block");
+        }
+
 
     }
+}
