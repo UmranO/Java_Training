6bad7b39cc6e5dff44614da2a120fe56b0dd0b72
Umran Ozman <umonal@yahoo.com>
2023-07-11 18:03:44 -0400

    TryCatchBlocks2 class updated

--- src/day37_exceptions/TryCatchBlocks2.java
+++ src/day37_exceptions/TryCatchBlocks2.java
@@ -1,4 +1,11 @@
 package day37_exceptions;
 
 public class TryCatchBlocks2 {
+    public static void main(String[] args) {
+        System.out.println("Program1 started");
+
+        int[] arr = {1, 2, 3, 4};
+
+    }
+
 }
