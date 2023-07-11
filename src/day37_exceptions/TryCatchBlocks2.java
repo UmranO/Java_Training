205ad7757e045f437f7b6e8f1631463384c9e549
Umran Ozman <umonal@yahoo.com>
2023-07-11 19:55:16 -0400

    TryCatchBlocks2 class updated

--- src/day37_exceptions/TryCatchBlocks2.java
+++ src/day37_exceptions/TryCatchBlocks2.java
@@ -2,9 +2,16 @@ package day37_exceptions;
 
 public class TryCatchBlocks2 {
     public static void main(String[] args) {
-        System.out.println("Program1 started");
 
+
+        System.out.println("Program1 started");
         int[] arr = {1, 2, 3, 4};
+            try{
+                System.out.println(arr[1000]);
+            } catch (RuntimeException e){
+
+            }
+        System.out.println("Program1 ended");
 
     }
 
