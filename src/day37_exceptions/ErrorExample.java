ea5134b6de70dbf08975159de9eab13e071fd432
Umran Ozman <umonal@yahoo.com>
2024-02-29 15:05:46 -0500

    ErrorExample Class updated

--- src/day37_exceptions/ErrorExample.java
+++ src/day37_exceptions/ErrorExample.java
@@ -1,4 +1,20 @@
 package day37_exceptions;
 
+import java.util.ArrayList;
+
 public class ErrorExample {
+
+    public static void main(String[] args) {
+
+        ArrayList<Pizza> pizzas = new ArrayList<>();
+
+        while(true){
+            pizzas.add(  new Pizza('S', 2, 3) );
+        }
+
+
+    }
+
 }
+
+//It will continue to create ppizza objects and at the end we'll receive OutOfMemoryError it will print: Java heap space
