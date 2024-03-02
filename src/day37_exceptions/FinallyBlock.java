c74c3cb8a5d6fd4f733e7aa61494c2f2413c2727
Umran Ozman <umonal@yahoo.com>
2024-03-02 14:05:33 -0500

    FinallyBlock Class updated

--- src/day37_exceptions/FinallyBlock.java
+++ src/day37_exceptions/FinallyBlock.java
@@ -17,3 +17,7 @@ package day37_exceptions;
             }
 
         }
+//Finally Block is always executed weather the exception is caught or not unless exit() is called in the catch block.
+//In fact there is n't a logic to call exit() since we are using the finally block to be sure that specific codes in it
+// are certainly be executed.
+//If it is caught the exit code is 0 if not caught exit code will be 1
