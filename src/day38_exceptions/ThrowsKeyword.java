3d96ab33238408bfba0f18f9f464a4c53632b197
Umran Ozman <umonal@yahoo.com>
2024-03-02 14:56:54 -0500

    ThrowsKeyword Class updated

--- src/day38_exceptions/ThrowsKeyword.java
+++ src/day38_exceptions/ThrowsKeyword.java
@@ -1,4 +1,22 @@
 package day38_exceptions;
 
 public class ThrowsKeyword {
+    public static void main(String[] args){
+
+        System.out.println("Program1 started");
+
+        // System.out.println(100/0);
+        try {
+            Thread.sleep(5000);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+
+        System.out.println("Program1 ended");
+
+        System.out.println("-------------------------------------------");
+
+
+    }
+
 }
