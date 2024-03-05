c46f4ca42bc29afebc0bd901496173168cad81fc
Umran Ozman <umonal@yahoo.com>
2024-03-05 11:51:32 -0500

    CustomExceptions Class updated

---
+++ src/day38_exceptions/CustomExceptions.java
@@ -0,0 +1,18 @@
+package day38_exceptions;
+
+class CustomCheckedException extends Exception{
+
+}
+
+class CustomUncheckedException extends RuntimeException{
+
+}
+
+
+public class CustomExceptions {                          //There can be only 1 public class in a Java file and that
+    public static void main (String [] args){            //class name should be same as the Java file.The other classes
+                                                         //(above) can't be public but we can have as many as we want
+
+
+    }
+}
