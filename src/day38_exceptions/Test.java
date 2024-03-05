7b04654f7bed96920e1a5c7aa294bd5f0b740083
Umran Ozman <umonal@yahoo.com>
2024-03-05 12:46:09 -0500

    Test Class updated

--- src/day38_exceptions/Test.java
+++ src/day38_exceptions/Test.java
@@ -1,13 +1,33 @@
 package day38_exceptions;
 import utilities.Library;
+
+
 public class Test {
     public static  void main(String[] args){
+
+        System.out.println("UO's-----------------------------");
+
         System.out.println("Hello World");
 
         //Thread.sleep(3500);                     //Built in sleep() from Thread Class-we have to give the argument in
                                                   //milliseconds + it will give us a checked exception so instead we can
-        Library.sleep(5.0);                 //use our custom() from Library Class/utilities so that we can give
+        Library.sleep(5.0);               //use our custom() from Library Class/utilities so that we can give
                                                   //the argument in seconds and the checked excep. is already handled
         System.out.println("Hello Cydeo");
+
+        System.out.println("M's-----------------------------");
+
+        System.out.println("Hello Cydeo");
+
+        // Thread.sleep(3500);
+        Library.sleep(3.5);
+
+        System.out.println("How are you today?");
+
+        System.out.println("--------------------------------------------------------------------------");
+
+
+
     }
+
 }
