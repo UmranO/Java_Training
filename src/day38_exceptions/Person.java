41c91a01f68358b26dc2e72a3bb4ac5769c3015e
Umran Ozman <umonal@yahoo.com>
2024-03-05 11:57:18 -0500

    Person Class updated

--- src/day38_exceptions/Person.java
+++ src/day38_exceptions/Person.java
@@ -1,4 +1,46 @@
 package day38_exceptions;
+import java.util.InputMismatchException;
 
 public class Person {
+
+    private String name;
+    private int age;
+    private final char gender;
+
+    public Person(String name, int age, char gender) {
+        setName(name);                                                   //age&name can be initialized in their Setters
+        setAge(age);
+        if(!(gender =='M' || gender =='F')){                             //if the gender is invalid
+            throw new RuntimeException("Invalid gender: "+gender);       //throws RuntimeException and crash the program
+        }                                                          //gender's final that's why it has to be initialized
+        this.gender = gender;                                      //in the constructor. It also has some conditions
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        if(age == 0){
+            throw new RuntimeException("Age has not been set");
+        }
+        return age;
+    }
+
+    public void setAge(int age) {
+        if(age < 0 || age > 150){                                           //if the given age is invalid
+            throw new RuntimeException("Invalid age: "+age);                //throw runtime exception, and crash program
+        }
+        this.age = age;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+
 }
