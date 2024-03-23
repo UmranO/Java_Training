020ef7715d65b21788d2e32d8dad7be60f2cfae0
Umran Ozman <umonal@yahoo.com>
2024-03-23 16:59:46 -0400

    ThreadHelloWorld Class updated

--- src/day39_collections/multiThreading/ThreadHelloWorld.java
+++ src/day39_collections/multiThreading/ThreadHelloWorld.java
@@ -1,4 +1,11 @@
 package day39_collections.multiThreading;
 
-public class ThreadHelloWorld {
+public class ThreadHelloWorld extends Thread{
+    @Override
+    public void run() {
+        for (int i=1; i<6; i++){
+            System.out.println("Hello world "+ i);;
+
+        }
+    }
 }
