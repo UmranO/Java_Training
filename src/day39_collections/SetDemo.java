50da47b6b7f2d9f5717d6f3b8d44e3053ceed316
Umran Ozman <umonal@yahoo.com>
2024-04-01 17:56:41 -0400

    SetDemo Class updated

--- src/day39_collections/SetDemo.java
+++ src/day39_collections/SetDemo.java
@@ -15,8 +15,9 @@ public class SetDemo {
         System.out.println(set1);
 
         System.out.println("-----------------------------------------------------------------------------------------");
-//But if it were a List it would accept the duplicate elements & also would maintain the insertion order & would also
-//accept more than 1 null elements
+
+        //But if it were a List it would accept the duplicate elements & also would maintain the insertion order &
+        //would also accept more than 1 null elements
         List<Integer> list=new ArrayList<>();
         list.addAll(Arrays.asList(10, 200, 300, 40, 90));           //accepts duplicates,has index,keeps insertion order
         list.addAll(Arrays.asList(10, 200, 300, 40, 90));
@@ -25,6 +26,8 @@ public class SetDemo {
         list.addAll(Arrays.asList(null, null, null, null, null));
 
         System.out.println(list);
+
+        System.out.println("-----------------------------------------------------------------------------------------");
 
     }
 }
