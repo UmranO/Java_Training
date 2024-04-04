fc036cac2c645ac47e1fb01f25dec91532d04eb7
Umran Ozman <umonal@yahoo.com>
2024-04-04 14:41:10 -0400

    CollectionPractice Class updated

--- src/day40_collections/CollectionPractice.java
+++ src/day40_collections/CollectionPractice.java
@@ -9,6 +9,8 @@ public class CollectionPractice {
         List <Integer> list3 = new Vector<>();
         List <Integer> list4 = new Stack<>();
 
+System.out.println("Collection can reference to the List Classes-----------------------------------------------------");
+
         Collection<Integer> collection = new ArrayList<>();
         collection.add(100);
         collection.addAll(Arrays.asList(100, 200, 100, 200, 300, 400, 500, 600, 700));
@@ -20,7 +22,28 @@ public class CollectionPractice {
                                                                //object type we need to downcast it to be able to use it
                                                                //Only after this part ((ArayList)collection)
                                                                //after downcasted we can call the object type's ()
+                                                             //Here we don't need ArrayList<Integer> ArrayList is enough
 
-        
+
+        //System.out.println(((Stack)collection).pop());   //Can't do it like this bec.it results in ClassCastException
+                                                           //This way we are trying to Stack to be a reference to an
+                                                           //ArrayList object (collection = new ArrayList<>();)
+                                                           //To be able to make reference type castings there should be
+                                                           //is A Relationship between the types.
+
+System.out.println("Collection can reference to the Set Classes--------------------------------------------------------");
+
+        Collection<Integer> collection2 = new HashSet<>();
+        collection2.addAll(Arrays.asList(100, 200, 100, 200, 300, 400, 500, 600, 700, 100, 100, 100));
+
+        System.out.println(collection2);
+
+        //System.out.println(( (ArrayList)collection2 ).get(4));  Can't cast to another type that doesn't have is A Rel.
+
+        System.out.println (  new ArrayList<>(collection2).get(4)  ); //If the ArrayList object is going to be used once
+                                                                      //no need to assign it to a variable
+        List<Integer> l = new ArrayList<>(collection2);               //But if it will be used more than once it's
+                                                                      //better to assign it.
+
     }
 }
