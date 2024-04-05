dcaa83d28ca02869dc25a485facc03d6d7f68f76
Umran Ozman <umonal@yahoo.com>
2024-04-05 12:50:58 -0400

    QueueDemo Class updated

--- src/day40_collections/QueueDemo.java
+++ src/day40_collections/QueueDemo.java
@@ -1,4 +1,30 @@
 package day40_collections;
+import java.util.*;
 
 public class QueueDemo {
+
+    public static void main (String[] args){
+
+System.out.println("--PriorityQueue----------------------------------------------------------------------------------");
+    Queue <Integer> queue1 = new PriorityQueue<>();  //Order is random (when we print it, otherwise it is prioritized
+    queue1.addAll(Arrays.asList(10,200,300,40,90));  //either in their natural order or with a custumized comparator) &
+    queue1.addAll(Arrays.asList(10,200,300,40,90));
+    queue1.addAll(Arrays.asList(10,200,300,40,90));
+  //queue1.addAll(Arrays.asList(null,null,null));    //doesn't accept null.--.Results in NullPointer Exception
+
+    System.out.println(queue1);
+
+
+    Queue <Integer> queue2 = new ArrayDeque<>();
+
+    Queue <Integer> queue3 = new LinkedList<>();
+
+        System.out.println("-----------------------------------------------------------------------------------------");
+
+        List<Integer> list = new LinkedList<>();            //LinkedList is both a Queue and a List.
+
+
+
+    }
+
 }
