d74b3a97f090dfd73611c46f1ffc4fc8cf4d28b1
Umran Ozman <umonal@yahoo.com>
2024-04-05 14:04:10 -0400

    QueueDemo Class updated

--- src/day40_collections/QueueDemo.java
+++ src/day40_collections/QueueDemo.java
@@ -7,12 +7,13 @@ public class QueueDemo {
 
 System.out.println("--PriorityQueue----------------------------------------------------------------------------------");
     Queue <Integer> queue1 = new PriorityQueue<>();  //Order is random (when we print it, otherwise it is prioritized
+
     queue1.addAll(Arrays.asList(10,200,300,40,90));  //either in their natural order or with a custumized comparator) &
     queue1.addAll(Arrays.asList(10,200,300,40,90));  //Allows duplicates
     queue1.addAll(Arrays.asList(10,200,300,40,90));
   //queue1.addAll(Arrays.asList(null,null,null));    //doesn't accept null.-->Results in NullPointer Exception
-
        System.out.println(queue1);
+
     queue1.poll();
     System.out.println(queue1);
     queue1.poll();
@@ -22,6 +23,7 @@ System.out.println("--PriorityQueue---------------------------------------------
 
 System.out.println("--ArrayDeque-------------------------------------------------------------------------------------");
     Queue <Integer> queue2 = new ArrayDeque<>();     //insertion order is preserved
+
     queue2.addAll(Arrays.asList(10,200,300,40,90));  //Allows duplicates
     queue2.addAll(Arrays.asList(10,200,300,40,90));
 
@@ -41,9 +43,6 @@ System.out.println("--ArrayDeque------------------------------------------------
         queue2.poll();
         queue2.poll();
         System.out.println(queue2);                 //Results in empty Queue []
-
-
-
 
 System.out.println("--LinkedList as a Queue--------------------------------------------------------------------------");
     Queue <Integer> queue3 = new LinkedList<>();
