4afe9c480274d96df40475e01d42fc461ea3c761
Umran Ozman <umonal@yahoo.com>
2024-04-23 21:34:13 -0400

    IterableExample Class updated

--- src/day40_collections/IterableExample.java
+++ src/day40_collections/IterableExample.java
@@ -18,12 +18,27 @@ System.out.println("--Collection's remove() by index #--------------------------
 
         for (int i = 0; i < list.size(); i++) {
             if (list.get(i) < 5) {
-                list.remove(i);                  //Not ConcurrentModificationException but didn't remove elements<5 
+                list.remove(i);                  //Not ConcurrentModificationException but didn't remove elements<5
             }
         }
 
         System.out.println(list);
 
+System.out.println("--In the loop we should use Iterator's remove()--------------------------------------------------");
 
+        List<Integer> list2 = new ArrayList<>();
+        list2.addAll(Arrays.asList(1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 6, 7, 1, 2, 3, 4, 5, 6, 7));
 
-    }}
+        Iterator<Integer> it = list2.iterator();        //1st we should create the Iterator object
+
+        while (it.hasNext()) {                          //2nd we should create the while loop to check if it has the
+            Integer each = it.next();                   //next element. If yes we get the next element.
+            if (each < 5) {                             //we filter with our condition
+                it.remove();                            //if the condition applies then we should remove that element
+            }                                           //since these 3 methods are correlated we don't need to give
+        }                                               //each in the () of remove method. We can call these () from
+                                                        //Iterator object not from the Collection object
+        System.out.println(list2);
+
+    }
+}
