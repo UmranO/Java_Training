59d7b3fb95c5a53d7ed10953a243a2c1d3789cd0
Umran Ozman <umonal@yahoo.com>
2024-04-24 10:55:27 -0400

    IterablePractice1 Class updated

--- src/day40_collections/IterablePractice1.java
+++ src/day40_collections/IterablePractice1.java
@@ -1,4 +1,22 @@
 package day40_collections;
+import java.util.*;
 
 public class IterablePractice1 {
-}
+    public static void main(String[] args) {
+
+        ArrayList<Integer> list = new ArrayList<>();
+        list.addAll(Arrays.asList(10));
+
+        Iterator<Integer> iterator = list.iterator();
+
+        System.out.println( iterator.hasNext() );                  //true
+        System.out.println(iterator.next());                       //10
+
+        System.out.println("-----------------------------------");
+
+
+
+
+    }
+    }
+
