dde471f8bc2c7b150d1aa5d688ccd528c4e6d30e
Umran Ozman <umonal@yahoo.com>
2024-04-24 12:03:29 -0400

    RemovePalindromes Class updated

--- src/day40_collections/RemovePalindromes.java
+++ src/day40_collections/RemovePalindromes.java
@@ -1,4 +1,35 @@
 package day40_collections;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Iterator;
+
 
 public class RemovePalindromes {
+    public static void main (String[] args){
+        List<String> names = new ArrayList<>();
+        names.addAll(
+                Arrays.asList("Java", "Python", "Cydeo", "Car", "Level", "civic", "radar", "kayak", "reviver", "racecar", "madam")
+        );
+
+System.out.println("--Remove the palindromes with Iterator-----------------------------------------------------------");
+
+        Iterator<String> it = names.iterator();
+
+        while(it.hasNext()){
+            String each = it.next();
+            String reverse = "";
+
+            for (int i = each.length()-1; i >= 0 ; i--) {
+                reverse += each.charAt(i);
+            }
+            if(each.equalsIgnoreCase(reverse)){
+                // names.remove(each);
+                it.remove();
+            }
+        }
+        System.out.println(names);
+
+        System.out.println("------------------------------------------------------");
+    }
 }
