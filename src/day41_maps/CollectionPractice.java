ce243534aaad7d1e5503781a4ed318ff3bd884de
Umran Ozman <umonal@yahoo.com>
2024-04-24 15:00:02 -0400

    CollectionPractice Class updated

--- src/day41_maps/CollectionPractice.java
+++ src/day41_maps/CollectionPractice.java
@@ -1,4 +1,17 @@
 package day41_maps;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class CollectionPractice {
+    public static void main(String[] args){
+
+        List<Integer> list1 = new ArrayList<>();
+        list1.addAll(Arrays.asList(1, 2, 3, 4, 5,6));
+
+        List<Integer> list2 = new ArrayList<>();
+        list2.addAll(Arrays.asList(7, 8, 9, 10, 11));
+
+
+    }
 }
