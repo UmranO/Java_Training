0f286e90b70ec393d6220daec0d0fd0139771704
Umran Ozman <umonal@yahoo.com>
2024-04-25 09:57:48 -0400

    ArrayPractice Class updated

--- src/day41_maps/ArrayPractice.java
+++ src/day41_maps/ArrayPractice.java
@@ -1,4 +1,22 @@
 package day41_maps;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class ArrayPractice {
+    public static void main(String[] args){
+
+        List<Integer> list1 = new ArrayList<>();
+        list1.addAll(Arrays.asList(1, 2, 3, 4, 5,6));
+
+        List<Integer> list2 = new ArrayList<>();
+        list2.addAll(Arrays.asList(7, 8, 9, 10, 11));
+
+        List<Integer> list3 = new ArrayList<>();
+        list3.addAll(Arrays.asList(70, 80, 90, 100, 110));
+
+
+
+    }
 }
