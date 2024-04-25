c6a4ac5209bc865238a46910baca9ee000c0bf31
Umran Ozman <umonal@yahoo.com>
2024-04-25 13:40:40 -0400

    ArrayPractice Class updated

--- src/day41_maps/ArrayPractice.java
+++ src/day41_maps/ArrayPractice.java
@@ -1,5 +1,4 @@
 package day41_maps;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -15,6 +14,21 @@ public class ArrayPractice {
 
         List<Integer> list3 = new ArrayList<>();
         list3.addAll(Arrays.asList(70, 80, 90, 100, 110));
+
+        // List<Integer>[] arrayOfListsSil ={list1,list2,list3};   =>Results in Generic Array Creation so when we create
+                                                                   //an Array it is possible if we create the Array as
+                                                                   //Raw Type and add the lists later. But this may be
+                                                                   //a problem -(type safety)
+System.out.println("--Arrays of Lists is possible--------------------------------------------------------------------");
+
+        List<Integer>[] arrayOfLists = new List [3];
+        arrayOfLists [0]=list1;
+        arrayOfLists [1]=list2;
+        arrayOfLists [2]=list3;
+
+        System.out.println(arrayOfLists [0].get(0));            //UO =>1
+        System.out.println(Arrays.toString(arrayOfLists));      //Since it's an Array we have to use toString() to print
+                                                     //=>[[1, 2, 3, 4, 5, 6], [7, 8, 9, 10, 11], [70, 80, 90, 100, 110]]
 
 
 
