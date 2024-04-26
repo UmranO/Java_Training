0b328818180066333f3fb40b5f15861928825768
Umran Ozman <umonal@yahoo.com>
2024-04-26 15:33:29 -0400

    MapClasses Class updated

--- src/day41_maps/MapClasses.java
+++ src/day41_maps/MapClasses.java
@@ -1,13 +1,27 @@
 package day41_maps;
-
 import java.util.*;
 
 public class MapClasses {
     public static void main(String[] args){
 
-        Map<String, Integer> hashMap = new HashMap<>();              //random order, accept null key & null values
-        Map<String, Integer> linkedHashMap = new LinkedHashMap<>();  //insertion order, accepts null key  & null values
-        Map<String, Integer> treeMap = new TreeMap<>();       //sorted order,doesn't accept null key,accepts null values
-        Map<String, Integer> hashtable = new Hashtable<>();   //random order,doesn't null key & null values,Synchronized
+System.out.println("--HashMap: Random order, accept null key & null values--------------------------------------------");
+        Map<String, Integer> hashMap = new HashMap<>();
+
+
+
+System.out.println("--LinkedHashMap: Insertion order, accepts null key  & null values---------------------------------");
+        Map<String, Integer> linkedHashMap = new LinkedHashMap<>();
+
+
+System.out.println("--TreeMap: Sorted order, doesn't accept null key, accepts null values-----------------------------");
+        Map<String, Integer> treeMap = new TreeMap<>();
+
+
+System.out.println("--HashTable: Random order, doesn't null key & null values, Synchronized---------------------------");
+        Map<String, Integer> hashtable = new Hashtable<>();
+
+
+
+
     }
 }
