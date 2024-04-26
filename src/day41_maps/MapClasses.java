cf999f97e1a47f2a8123e0e7fa88dfdc8c7ff239
Umran Ozman <umonal@yahoo.com>
2024-04-26 15:27:23 -0400

    MapClasses Class updated

--- src/day41_maps/MapClasses.java
+++ src/day41_maps/MapClasses.java
@@ -1,4 +1,13 @@
 package day41_maps;
 
+import java.util.*;
+
 public class MapClasses {
+    public static void main(String[] args){
+
+        Map<String, Integer> hashMap = new HashMap<>();              //random order, accept null key & null values
+        Map<String, Integer> linkedHashMap = new LinkedHashMap<>();  //insertion order, accepts null key  & null values
+        Map<String, Integer> treeMap = new TreeMap<>();       //sorted order,doesn't accept null key,accepts null values
+        Map<String, Integer> hashtable = new Hashtable<>();   //random order,doesn't null key & null values,Synchronized
+    }
 }
