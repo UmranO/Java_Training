b36011b042fc60a0d48ed0bff2f0641572efdba3
Umran Ozman <umonal@yahoo.com>
2024-08-20 12:42:50 -0400

    MapClasses Class updated

--- src/day41_maps/MapClasses.java
+++ src/day41_maps/MapClasses.java
@@ -24,7 +24,7 @@ System.out.println("--HashMap: Random order, accept null key & null values------
             hashMap.put("Daniel", 95000);    //If you try to add the same key it will not accept it.
             hashMap.put("Emily", 100000);
             hashMap.put("Bella", 85000);
-        //  hashMap.put("Daniel", 80000);      If you give different value for the same key then it will keep the last
+        //  hashMap.put("Daniel", 80000);    If you give different value for the same key then it will keep the last
                                              //given value bec. it will update the value.
             System.out.println(hashMap);     //Console:{Emily=100000, Daniel=95000, Bella=85000}
 
@@ -33,19 +33,30 @@ System.out.println("--HashMap: Random order, accept null key & null values------
             System.out.println(hashMap);     //Console:{Emily=100000, Arron=78000, Daniel=95000, Bella=85000}
 
             hashMap.put("Chris", null);      //null value is accepted in HashMap
-            hashMap.put("Breanna", null);    //null values can be duplicated in HashMap
+            hashMap.put("Breanna", null);    //more than 1 null value can be added in HashMap
             hashMap.put(null, 120000);       //null key is accepted in HashMap
-            hashMap.put(null, 110000);       //only 1 null key will be accepted. And since
-            hashMap.put(null, 100000);       //And since null key is the last one this will be kept only
+            hashMap.put(null, 110000);       //only 1 null key will be accepted.
+            hashMap.put(null, 100000);       //And since this null key is the last one, this will be kept only
 
- System.out.println("--LinkedHashMap: Insertion order, accepts null key  & null values---------------------------------");
+            System.out.println(hashMap);
+            //Console:{null=100000, Emily=100000, Breanna=null, Chris=null, Arron=78000,Daniel=95000, Bella=85000}
 
+System.out.println("--LinkedHashMap: Insertion order, accepts null key  & null values---------------------------------");
             Map<String, Integer> linkedHashMap = new LinkedHashMap<>();
             linkedHashMap.put("Daniel", 95000);
             linkedHashMap.put("Emily", 100000);
             linkedHashMap.put("Bella", 85000);
+            //map1.put("Daniel", 80000);
+            linkedHashMap.put("Arron", 78000);
+            linkedHashMap.put("Chris", null);     //null value is accepted in LinkedHashMap
+            linkedHashMap.put("Breanna", null);   //more than 1 null value can be added in the LinkedHashMap
+            linkedHashMap.put(null, 120000);      //null key is accepted in LinkedHashMap
+            linkedHashMap.put(null, 110000);      //only 1 null key will be accepted.
+            linkedHashMap.put(null, 100000);      //And since this null key is the last one, this will be kept only
 
-
+        System.out.println("linkedHashMap = " + linkedHashMap);
+ //Console:linkedHashMap = {Daniel=95000, Emily=100000, Bella=85000, Arron=78000, Chris=null, Breanna=null, null=100000}
+ //In LinkedHashMap it does keep the insertion order. 1st added element will be the 1st element of the LinkedHashMap
 
 
 
