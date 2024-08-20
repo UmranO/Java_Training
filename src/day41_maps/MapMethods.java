66d45948fb4d34eba6e8b5db4b707f68921377a1
Umran Ozman <umonal@yahoo.com>
2024-08-20 18:44:01 -0400

    MapMethods Class updated

--- src/day41_maps/MapMethods.java
+++ src/day41_maps/MapMethods.java
@@ -4,11 +4,11 @@ import java.util.*;
 public class MapMethods {
     public static void main(String[] args){
 
-        Map<String,String> map = new TreeMap<> ();   //put() is used to adding the key and value into the map as a pair
+        Map<String,String> map = new TreeMap<>();   //put() is used to adding the key and value into the map as a pair
 
 //-put(key,value)-- is used to adding the key and value into the map as a pair------------------------------------------
-               //-- can also be used for updating the values of existing keys but the repalce() is faster bec it doesn't
-               //--have the intention to insert
+        //-- can also be used for updating the values of existing keys but the repalce() is faster bec it doesn't
+        //--have the intention to insert
 
         map.put("C02", "Josh");
         map.put("B03", "Nora");
@@ -22,7 +22,7 @@ public class MapMethods {
 
 //-size()--counts / returns the # of pairs that are stored in the Map---------------------------------------------------
         System.out.println(map);
-        System.out.println( map.size());                                    //=> 9
+        System.out.println(map.size());                                    //=> 9
 
 //-get(key)--returns the value of the specified key---------------------------------------------------------------------
         System.out.println(map.get("A04"));                                 //=> Tatiana
@@ -41,7 +41,7 @@ public class MapMethods {
 
 //-containsKey(key)--returns true if the specified key is included in the Map, otherwise returns false------------------
         System.out.println(map.containsKey("B03"));                       // =>true
-        System.out.println( map.containsKey("H98"));                      // =>false
+        System.out.println(map.containsKey("H98"));                       // =>false
 
 //-containsValue(value)--returns true if the specified value is included in the Map, otherwise returns false------------
         System.out.println(map.containsValue("Lucy"));                    // =>false
@@ -54,21 +54,23 @@ public class MapMethods {
         System.out.println(map.isEmpty());
 
 //-equals(MapObject)--checks if the map is equal to the given map in the arguement, returns boolean---------------------
-        Map<String,String> map1 = new LinkedHashMap<>();  map1.put("C02", "Josh"); map1.put("B03", "Nora");
+        Map<String, String> map1 = new LinkedHashMap<>();
+        map1.put("C02", "Josh");
+        map1.put("B03", "Nora");
         map1.put("A02", "Khashayar");
-        Map<String,String> map2 = new LinkedHashMap<>(); map2.put("A02", "Khashayar");map2.put("C02", "Josh");
+        Map<String, String> map2 = new LinkedHashMap<>();
+        map2.put("A02", "Khashayar");
+        map2.put("C02", "Josh");
         map2.put("B03", "Nora");
 
-        System.out.println(map1.equals(map2)+"************");
+        System.out.println(map1.equals(map2) + "************");
 
 //-clear()--removes all the keys & values from the map, size will be set to 0-------------------------------------------
         map.clear();
-        System.out.println(map);                                         //=>{}
-
+        System.out.println(map);                                            //=>{}}
 
   }
 }
-
 /*
 pair: id - name
 I want it to be sorted by id so therefor we can use TreeMap
