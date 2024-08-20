ef4c3063e167adb4da53dd06dfed4aa5d7613666
Umran Ozman <umonal@yahoo.com>
2024-08-20 16:01:18 -0400

    MapMethods Class updated

--- src/day41_maps/MapMethods.java
+++ src/day41_maps/MapMethods.java
@@ -1,4 +1,20 @@
 package day41_maps;
-
+import java.util.Map;
+import java.util.TreeMap;
 public class MapMethods {
+    public static void main(String[] args){
+
+        Map<String,String> map = new TreeMap<> ();
+
+        map.put("C02", "Josh");
+        map.put("B03", "Nora");
+        map.put("A02", "Khashayar");
+        map.put("D02", "Abidullah");
+        map.put("A03", "Tatiana");
+        map.put("A04", "Tatiana");
+        map.put("A05", "Tatiana");
+        map.put("A06", "Tatiana");
+        map.put("A07", "Tatiana");
+
+    }
 }
