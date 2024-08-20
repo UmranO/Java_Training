525cf7448c64eab20a55a77b99d3936eae1a3c70
Umran Ozman <umonal@yahoo.com>
2024-08-20 13:54:23 -0400

    MapClasses Class updated

--- src/day41_maps/MapClasses.java
+++ src/day41_maps/MapClasses.java
@@ -43,7 +43,7 @@ System.out.println("--HashMap: Random order, accept null key & null values------
          System.out.println(hashMap);
             //Console:{null=100000, Emily=100000, Breanna=null, Chris=null, Arron=78000,Daniel=95000, Bella=85000}
             //Order is Random in HashMap. It doesn't keep insertion Order.
-        
+
 System.out.println("--LinkedHashMap: Insertion order, accepts null key  & null values---------------------------------");
             Map<String, Integer> linkedHashMap = new LinkedHashMap<>();
 
@@ -91,9 +91,35 @@ System.out.println("--HashTable: Random order, doesn't null key & null values, S
         // hashtable.put(null, 120000);         Key in Hashtable can not be null!    Results in NullPointerException
         // hashtable.put(null, 110000);         Key in Hashtable can not be null!    Results in NullPointerException
 
-        System.out.println("hashtable = " + hashtable);
+        System.out.println("1-hashtable = " + hashtable);
         //Console:hashtable = {Arron=78000, Daniel=95000, Emily=100000, Bella=85000}
         //Order is Random in HashTable It doesn't keep insertion Order.
+
+       // hashtable.put(null, 100000);                Soru:Can it be handled with Try& Catch? M:Yes
+
+         try {
+            hashtable.put(null, 100000);
+        } catch (RuntimeException e){
+            e.printStackTrace();
+        }
+
+        System.out.println("2-hashtable = " + hashtable);
+
+        //Console:hashtable = {Arron=78000, Daniel=95000, Emily=100000, Bella=85000}
+        //Now the hashTable will be printed and the exit code will be 0
+        //but the pair which is causing the Exception (null, 100000) won't be added to the Map. To fix this we should go
+        //back & change the null to eg "Diana"
+
+        try{
+            hashtable.put("Diana", 100000);
+        } catch(RuntimeException e){
+            e.printStackTrace();
+        }
+        System.out.println("3-hashtable = " + hashtable);
+        //Now the Pair will be added to the hashtable and it will be printed in the console. Exit code:0
+        //Console: hashtable = {Arron=78000, Daniel=95000, Diana=100000, Emily=100000, Bella=85000}
+
+
 /*
 pair: name & salary
 Daniel=95000;
