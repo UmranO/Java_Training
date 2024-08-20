5de877fc92a94f02cbd422042f37294ae3b95d2e
Umran Ozman <umonal@yahoo.com>
2024-08-20 12:06:35 -0400

    MapClasses Class updated

--- src/day41_maps/MapClasses.java
+++ src/day41_maps/MapClasses.java
@@ -24,11 +24,11 @@ public class MapClasses {
 System.out.println("--HashMap: Random order, accept null key & null values--------------------------------------------");
             Map<String, Integer> hashmap=new HashMap<>();
             hashMap.put("Daniel", 95000);
+            hashMap.put("Daniel", 95000);    //If you try to add the same key it will not accept it. If you give
+            hashMap.put("Emily", 100000);    // different value for the same key then it will keep the last given value
+            hashMap.put("Bella", 85000);     //bec. it will update the value.
             hashMap.put("Daniel", 95000);
-            hashMap.put("Emily", 100000);
-            hashMap.put("Bella", 85000);
-            hashMap.put("Daniel", 95000);
-            System.out.println(hashMap);
+            System.out.println(hashMap);     //Console:{Emily=100000, Daniel=95000, Bella=85000}
 /*
 Daniel=95000;
 Emily=100000;
