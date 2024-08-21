f76efca7700b16b3a127ea343568a2e8edbcb3b7
Umran Ozman <umonal@yahoo.com>
2024-08-21 17:19:26 -0400

    IteratingMaps Class updated

--- src/day42_maps/IteratingMaps.java
+++ src/day42_maps/IteratingMaps.java
@@ -16,7 +16,7 @@ public class IteratingMaps {
 
         System.out.println(map);
 
-System.out.println("---Iterating the map by the keys---------------------------------------------");
+System.out.println("---Iterating the map by the keys----------------------------------------------------------------");
 
     for (String eachK: map.keySet()){
     System.out.println(eachK);
@@ -29,7 +29,7 @@ System.out.println("---Iterating the map by the keys----------------------------
         }
         System.out.println(map);
 
-        System.out.println("---Iterating the map by the values------------------------------------------");
+        System.out.println("---Iterating the map by the values-------------------------------------------------------");
 
         for(Integer eachValue: map.values()){
             System.out.println(eachValue);                  //=> prints each value of the map
@@ -47,13 +47,17 @@ System.out.println("---Iterating the map by the keys----------------------------
         }
         System.out.println(max);
 
-        System.out.println("---Iterating the map by the entries/pairs-----------------------------------");
+ System.out.println("---Iterating the map by the entries/pairs-------------------------------------------------------");
 
         for (Map.Entry<String, Integer> eachEntry : map.entrySet()){
-            System.out.println(eachEntry);
+            System.out.println(eachEntry);                   //=> prints each entry of the map
         }
-        System.out.println("-------------------------------------");
 
+System.out.println("---If you can get each entry then you can get each key and each value separately:----------------");
+
+        for (Map.Entry<String, Integer> eachEnt : map.entrySet()){
+            System.out.println("Key= " +  eachEnt.getKey() + "  Value= "+ eachEnt.getValue());
+        }
 
     }
 }
