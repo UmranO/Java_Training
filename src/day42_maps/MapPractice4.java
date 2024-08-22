4fd97ea82ad1969303c99244378a4b9fd6994391
Umran Ozman <umonal@yahoo.com>
2024-08-22 10:49:33 -0400

    MapPractice4 Class updated

--- src/day42_maps/MapPractice4.java
+++ src/day42_maps/MapPractice4.java
@@ -1,4 +1,18 @@
 package day42_maps;
+import java.util.Map;
+import java.util.LinkedHashMap;
+import java.time.LocalDate;
 
 public class MapPractice4 {
+    public static void main(String[] args) {
+        Map<String, Object> person1 = new LinkedHashMap<>();
+        person1.put("name", "Arthur");
+        person1.put("gender", 'M');
+        person1.put("age", 32);
+        person1.put("job_title", "Developer");
+        person1.put("salary", 100000);
+        person1.put("hired_date", LocalDate.of(2021, 1, 15));
+        person1.put("married", true);
+
+    }
 }
